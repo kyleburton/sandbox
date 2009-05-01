67f4c5562711de28ccad0fa6a3b18650e3efc524
Kyle Burton <mortis@indigo64.(none)>
2009-04-30 20:44:53 -0400

    added proj-file for finding the location of data files

--- del.icio.us/src/clj/com/github/kyleburton/sandbox/delicous.clj
+++ del.icio.us/src/clj/com/github/kyleburton/sandbox/delicous.clj
@@ -1,5 +1,3 @@
-;; 
-
 (ns com.github.kyleburton.sandbox.delicious
   (:require [clojure.contrib.duck-streams :as ds])
   (:require [ clojure.contrib.str-utils :as str-utils]))
@@ -15,7 +13,17 @@
     (str *this-dir* fname)
     (str *this-dir* "/" fname)))
 
-(def *bookmarks-file* (relative-file "../../data/delicious-20090429.htm"))
+(def *proj-root*
+     (loop [here (ds/file *this-dir*)]
+       (if (= "src" (.getName here))
+         (.getParent here)
+         (recur (ds/file (.getParent here))))))
+
+(defn proj-file [fname]
+  (.replaceAll (str *proj-root* "/" fname)
+               "//" "/"))
+
+(def *bookmarks-file* (proj-file "/data/delicious-20090429.htm"))
 
 ;; "<DT><A HREF=\"http://hopper.squarespace.com/blog/2008/6/22/introducing-shovel-an-amqp-relay.html\" LAST_VISIT=\"1239821460\" ADD_DATE=\"1239821460\" TAGS=\"shovel,erlang,rabbit-mq,messaging,amqp\">No Clumps, Fisheyes or Microgels  -  Blog   - Introducing Shovel: An AMQP Relay</A>"
 (defn parse-line [#^String line]
@@ -28,7 +36,6 @@
       :description description})
    (catch Exception ex
      (prn (format "unable to parse line='%s' ex=%s" line ex)))))
-
 
 ;; (parse-line (first (filter #(.startsWith %1 "<DT>") (line-seq (ds/reader *bookmarks-file*)))))
 
@@ -44,7 +51,7 @@
 
 
 
-(dump-to-tab (relative-file "../../delicious.tab"))
+(dump-to-tab (proj-file "delicious.tab"))
 
 (map parse-line (filter #(.startsWith %1 "<DT>") (line-seq (ds/reader *bookmarks-file*))))
 
