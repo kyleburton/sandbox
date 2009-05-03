a934b421f3848e2e754b3151805b944d7c7aa73d
Kyle R. Burton <kburton@healthmarketscience.com>
2009-05-03 13:52:45 -0400

    list-all, files and directories

--- clojure-utils/src/clj/com/github/kyleburton/sandbox/ftp.clj
+++ clojure-utils/src/clj/com/github/kyleburton/sandbox/ftp.clj
@@ -1,9 +1,13 @@
+;; simple wrappers for jakarta's ftp client
+
+;; TODO: keep checking reply codes, throw exception if not OK
+
 (ns com.github.kyleburton.sandbox.ftp
   (:import (org.apache.commons.net.ftp FTP FTPClient)
            (java.net URL))
   (:use [ clojure.contrib.str-utils :as str]))
 
-(defmulti open class)
+(defmulti open "Open an ftp connection." class)
 
 (defmethod open String [s]
   (open (java.net.URL. s)))
@@ -26,9 +30,29 @@
        (let [[uname# pass#] (.split (.getUserInfo u#) ":" 2)]
          (.login ~client uname# pass#)))
      (.changeWorkingDirectory ~client (.getPath u#))
+     (.setFileType ~client FTP/BINARY_FILE_TYPE)
      (reset! res# 
             (do
               ~@body))
      (.disconnect ~client)
      @res#))
+
+(defn list-all [url]
+  (with-ftp [client url]
+    (map #(.getName %) (.listFiles client))))
+
+(defn list-files [url]
+  (with-ftp [client url]
+    (map #(.getName %) (filter #(.isFile %) (.listFiles client)))))
+
+(defn list-directories [url]
+  (with-ftp [client url]
+    (map #(.getName %) (filter #(.isDirectory %) (.listFiles client)))))
+
+(defn retrieve-file [url fname & [local-file]]
+  (with-ftp [client url]
+    (if local-file
+      (with-open [outstream (java.io.FileOutputStream. local-file)]
+        (.retrieveFile client fname outstream))
+      (.retrieveFile client fname))))
 
