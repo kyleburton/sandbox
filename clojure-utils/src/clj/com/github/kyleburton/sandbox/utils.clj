6a503d7e24e2fc79b0518092710ba7553b2c3d5e
Kyle Burton <kyle.burton@gmail.com>
2009-05-04 12:01:30 -0400

    docstrings and handle string and re in find-all

--- clojure-utils/src/clj/com/github/kyleburton/sandbox/utils.clj
+++ clojure-utils/src/clj/com/github/kyleburton/sandbox/utils.clj
@@ -1,15 +1,33 @@
 (ns com.github.kyleburton.sandbox.utils)
 
-(defn raise [args]
+(defn raise 
+  "Simple wrapper around throw."
+  [args]
   (throw (RuntimeException. (apply format args))))
 
-(defn- get-user-home []
+(defn- log [& args]
+  (prn (apply format args)))
+
+(defn #^String get-user-home
+  "Get the user's home dir as a string."
+  []
   (System/getProperty "user.home"))
 
-(defn $HOME [& paths]
-  (->file (apply str (cons (str (System/getProperty "user.home") "/") (apply str (interpose "/" paths))))))
+(defn #^java.io.File ->file
+  "Coerce into a File."
+  [thing]
+  (java.io.File. (str thing)))
 
-(defmulti expand-file-name class)
+(defn #^java.io.File $HOME
+  "Construct a path relative to the user's home directory."
+  [& paths]
+  (->file (apply str 
+                 (cons (str (System/getProperty "user.home") "/") 
+                       (apply str (interpose "/" paths))))))
+
+(defmulti expand-file-name
+  "Perform bash style expansion on the given path.  Eg: ~/file.txt."
+  class)
 
 (defmethod expand-file-name String [#^String path]
   (cond (.startsWith path "~/")
@@ -20,17 +38,23 @@
         path))
 
 
-(defn mkdir [path]
+(defn mkdir
+  "Create the given directory path, fall back gracefuly if the path
+  exists, warning if it's not a directory."
+  [path]
   (let [f (->file path)]
     (if (not (.exists f))
       (do
         (log "[INFO] mkdir: creating %s" path)
-        (.mkdirs f))
+        (.mkdirs f)
+        true)
       (if (not (.isDirectory f))
         (log "[WARN] mkdir: %s exists and is not a directory!")
         (log "[DEBUG] mkdir: exists: %s" path)))))
 
-(defn drain-line-reader [rdr]
+(defn drain-line-reader 
+  "Drain a buffered reader into a sequence."
+  [#^java.io.BufferedReader rdr]
   (loop [res []
          line (.readLine rdr)]
     (if line
@@ -38,14 +62,18 @@
              (.readLine rdr))
       res)))
 
-(defn exec [cmd]
+(defn exec
+  "Simple wrapper around Runtime.exec - not intended to compete with clojure.contrib.shell-out"
+  [cmd]
   (let [proc (.exec (Runtime/getRuntime) cmd)
         rv (.waitFor proc)]
     {:error (drain-line-reader (java.io.BufferedReader. (java.io.InputStreamReader. (.getErrorStream proc))))
      :output (drain-line-reader (java.io.BufferedReader. (java.io.InputStreamReader. (.getInputStream proc))))
      :exit rv}))
 
-(defn symlink [src dst]
+(defn symlink
+  "Create a symlink."
+  [src dst]
   (let [src (->file src)
         dst (->file dst)]
     (if (not (.exists src))
@@ -58,12 +86,16 @@
         (if (not (= 0 (:exit res)))
           (log "[ERROR] %s" (:error res)))))))
 
-(defn delete [path]
+(defn delete
+  "Remove a file if it exists."
+  [path]
   (let [path (->file path)]
     (if (.exists path)
       (.delete path))))
 
-(defn url-get [url]
+(defn url-get 
+  "Very simplistic retreival of a url target."
+  [url]
   (with-open [is (.openStream (java.net.URL. url))]
     (loop [sb (StringBuffer.)
            chr (.read is)]
@@ -74,7 +106,9 @@
           (recur sb
                  (.read is)))))))
 
-(defn url-download [url target-dir]
+(defn url-download
+  "Shell's out to wget to pull the file into the target directory."
+  [url #^String target-dir]
   (let [cmd (format "wget -P %s -c %s" target-dir url)
         res (exec cmd)]
     (log "[INFO] wget: %s" cmd)
@@ -82,20 +116,26 @@
       (log "[ERROR] %s" (:error res)))))
 
 
-(defn- all-groups [m]
+(defn all-groups 
+  "Extracts all the groups from a java.util.regex.Matcher into a seq."
+  [#^java.util.regex.Matcher m]
   (for [grp (range 1 (+ 1 (.groupCount m)))]
     (.group m grp)))
 
 
-(defn re-find-all [re str]
+(defn re-find-all
+  "Retreive all of the matches for a regex in a given string."
+  [re str]
   (doall
-   (loop [m (re-matcher re str)
+   (loop [m (re-matcher (if (isa? (class re) String) (re-pattern re) re) str)
           res []]
      (if (.find m)
        (recur m (conj res (vec (all-groups m))))
        res))))
 
-(defn chmod [perms file]
+(defn chmod
+  "Change a file or directory's permissions.  Shells out to perform the chmod."
+  [perms file]
   (let [cmd (format "chmod %s %s" perms file)
         res (exec cmd)]
     (log "[INFO] chmod: %s" cmd)
