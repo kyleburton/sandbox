5c222b93630fdf51796d397e9d658600845188e8
Kyle Burton <kyle.burton@gmail.com>
2009-05-07 12:30:54 -0400

    comments and simplified filters

--- clojure-utils/src/clj/com/github/kyleburton/sandbox/mvn.clj
+++ clojure-utils/src/clj/com/github/kyleburton/sandbox/mvn.clj
@@ -3,17 +3,23 @@
 ;;
 ;;
 ;; (map :name (mvn-plugins))
+
 ;; => ("clean" "compiler" "deploy" "install" "resources" "site"
-;;     "surefire" "verifier" "Packaging types / tools" "ear" "ejb" "jar"
-;;     "rar" "war" "shade" "Reporting plugins" "changelog" "changes"
-;;     "checkstyle" "clover" "doap" "docck" "javadoc" "jxr" "pmd"
-;;     "project-info-reports" "surefire-report" "Tools" "ant" "antrun"
-;;     "archetype" "assembly" "dependency" "enforcer" "gpg" "help"
-;;     "invoker" "one" "patch" "plugin" "release" "reactor"
-;;     "remote-resources" "repository" "scm" "source" "stage" "IDEs"
-;;     "eclipse" "idea")
-;; (map :name (mvn-plugin-goals "release"))
+;;     "surefire" "verifier" "ear" "ejb" "jar" "rar" "war" "shade"
+;;     "changelog" "changes" "checkstyle" "clover" "doap" "docck"
+;;     "javadoc" "jxr" "pmd" "project-info-reports" "surefire-report"
+;;     "ant" "antrun" "archetype" "assembly" "dependency" "enforcer" "gpg"
+;;     "help" "invoker" "one" "patch" "plugin" "release" "reactor"
+;;     "remote-resources" "repository" "scm" "source" "stage" "eclipse"
+;;     "idea")
+
 ;;
+;; (map :name (mvn-plugin-goals "release"))
+;; => ("release:branch" "release:clean" "release:help" "release:perform"
+;;     "release:prepare" "release:rollback" "release:stage")
+;; 
+;;;; (map :name (mvn-plugin-goals "dependency"))
+;; (filter #(.contains (:name %) "copy") (mvn-plugin-goals "dependency"))
 
 (ns com.github.kyleburton.sandbox.mvn
   (:require [com.github.kyleburton.sandbox.utils :as kutils]
@@ -29,33 +35,28 @@
   (web/get->string (or url *mvn-plugins-url*)))
 
 (def-once-only mvn-plugins-grid []
-  (filter
-   (fn [row]
-     (.contains (nth row 0)
-                "href"))
-   (drop 2
-         (lparse/html-table->matrix 
-          (first
-           (filter
-            ;; NB: this hard-coded string might go away at some point...
-            #(.contains % "Clean up after the build.")
-            (lparse/html->tables (mvn-plugins-html))))))))
+  (filter #(and (not (empty? %)) (.contains (nth % 0) "href"))
+   (lparse/html-table->matrix 
+    (first
+     (filter
+      ;; NB: this hard-coded string might go away at some point...
+      #(.contains % "Clean up after the build.")
+      (lparse/html->tables (mvn-plugins-html)))))))
 
 (def-once-only mvn-plugins []
-  (let [matrix (mvn-plugins-grid)]
-    (map (fn [row]
-           {
-            :name              (.trim (web/strip-html (nth row 0)))
-            :url               (first (lparse/html->links (nth row 0)))
-            :version           (nth row 1)
-            :release-date      (nth row 2)
-            :description       (nth row 3)
-            :source-repo       (first (lparse/html->links (nth row 4)))
-            :issue-tracking    (first (lparse/html->links (nth row 5)))
-            })
-         matrix)))
+  (map (fn [row]
+         {
+          :name              (.trim (web/strip-html (nth row 0)))
+          :url               (first (lparse/html->links (nth row 0)))
+          :version           (nth row 1)
+          :release-date      (nth row 2)
+          :description       (nth row 3)
+          :source-repo       (first (lparse/html->links (nth row 4)))
+          :issue-tracking    (first (lparse/html->links (nth row 5)))
+          })
+       (mvn-plugins-grid)))
 
-(map :name (mvn-plugins))
+;; (map :name (mvn-plugins))
 
 (defn mvn-plugin-goals [plugin-name]
   (let [entry (first (filter #(= plugin-name (:name %))
@@ -70,12 +71,12 @@
     (map (fn [row]
            {
             :name        (.trim (web/strip-html (nth row 0)))
-            :uri         (first (lparse/html->links (nth row 0)))
+            :url         (first (lparse/html->links (nth row 0)))
+            :plugin-url  goal-url
             :description (nth row 1);(web/html-decode (nth row 1))
             })
          goal-table)))
 
-
-
-
-
+;; (filter  (mvn-plugin-goals "dependency"))
+;; (map :plugin-url (mvn-plugin-goals "dependency"))
+;; (filter #(.contains (:name %) "dependency") (mvn-plugins))
