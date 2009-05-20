3be1b4789e0d73c674dc57f32180c76a9b11ea55
Kyle Burton <mortis@indigo64.(none)>
2009-05-19 21:34:25 -0400

    indentation

--- clojure-utils/src/clj/com/github/kyleburton/sandbox/log4j.clj
+++ clojure-utils/src/clj/com/github/kyleburton/sandbox/log4j.clj
@@ -1,6 +1,9 @@
+;; TODO: switch to commons-logging
+;; commons-logging.properties
 (ns com.github.kyleburton.sandbox.log4j
   (:import
-   (org.apache.log4j Logger PropertyConfigurator))
+   (org.apache.log4j Logger PropertyConfigurator)
+   (org.apache.commons.logging Log LogFactory))
   (:require [com.github.kyleburton.sandbox.utils :as kutils])
   (:use [clojure.contrib.str-utils :as str]))
 
@@ -19,15 +22,20 @@
       (reset! *configured* true))))
 
 (defn logger [category]
+  ;; TODO: move the ensure out of here, initialization should be a
+  ;; concern handled outside this moudle...
   (ensure-configured)
   (Logger/getLogger category))
 
+;; TODO: wrap the expansions with if(Log.isDebug){...} to avoid
+;; argument evaluation when logging is otherwise disabled, keep that
+;; hidden in behind the macros...
 (defmacro LOG []
   `(do (def ~'*log* (logger (.toString ~'*ns*)))
        (defn ~'log-fatal [& args#] (.fatal ~'*log* (str args#)))
        (defn ~'log-error [& args#] (.error ~'*log* (str args#)))
-       (defn ~'log-warn [& args#]  (.warn ~'*log* (str args#)))
-       (defn ~'log-info [& args#]  (.info ~'*log* (str args#)))
+       (defn ~'log-warn  [& args#] (.warn ~'*log*  (str args#)))
+       (defn ~'log-info  [& args#] (.info ~'*log*  (str args#)))
        (defn ~'log-debug [& args#] (.debug ~'*log* (str args#)))
        (defn ~'log-trace [& args#] (.trace ~'*log* (str args#)))))
 
