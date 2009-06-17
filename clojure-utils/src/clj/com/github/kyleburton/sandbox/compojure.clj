e0476f727997d34078c0481987d314719d2f2446
Kyle Burton <mortis@indigo64.(none)>
2009-06-17 00:18:09 -0400

    included stylesheet

--- clojure-utils/src/clj/com/github/kyleburton/sandbox/compojure.clj
+++ clojure-utils/src/clj/com/github/kyleburton/sandbox/compojure.clj
@@ -1,21 +1,25 @@
 (ns com.github.kyleburton.sandbox.compojure
   (:use compojure))
 
-
+(defn resource-link [lnk]
+  (format "%s?%s" lnk (.getTime (java.util.Date.))))
 (defn template-page [request title & body]
   (let [title (str "Compojure App - " title)]
-    (html [:head [:title title]] 
-          [:h1 title]
+    (html [:head [:title title]
+           [:link {:href "/stylesheet/app.css?3"
+                   :rel "stylesheet"
+                   :type "text/css"}]] 
+          [:div {:id "header"}
+           [:h1 title]]
           (if-let [flash (-> request :mailx-params :flash)]
-            [:div {:style "background: #FEE"}
+            [:div {:id "flash"}
              flash])
-          body
-          [:br]
-          [:br]
-          [:div {:style "text-align: center;"}
+          [:div {:id "main-body"} body]
+          [:div {:id "footer"}
            [:a {:href "/"} "Home"]
-           "|" [:a {:href "/items"}     "Items"]
-           "|" [:a {:href "/about"}     "About"]])))
+           " | " [:a {:href "/items"}     "Items"]
+           " | " [:a {:href "/about"}     "About"]
+           " | " [:a {:href "http://github.com/weavejester/compojure/tree/master"} "Compojure"]])))
 
 (defn index-page [request]
   (template-page request "Home"
@@ -47,11 +51,52 @@
 ;;       [:div "Error, page not found."]
 ;;       [:pre (str request)]))
 
+(defn stylesheet-page [request]
+  {:status 200
+   :headers { "Content-Type" "text/css" }
+   :body "
+body { 
+  background: #EEE;
+}
+
+#main-body {
+  margin-left: 30%;
+  margin-right: 30%;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  border-style: solid;
+  border-width: 2px;
+  border-color: #AAA;
+  padding: 0.5em;
+}
+
+#header {
+  text-align: left;
+  background: #CCC;
+  padding: 1em;
+  margin-left: 30%;
+  margin-right: 30%;
+}
+
+#footer {
+  text-align: center;
+  background: #CCC;
+  margin-top: 1em;
+  padding: 0.5em;
+  margin-left: 30%;
+  margin-right: 30%;
+}
+
+"})
+
+;(stylesheet-page 0)
+
 (defroutes my-app
   (GET  "/"                      index-page)
   (GET  "/items"                 items-page)
   (GET  "/item/:id"              item-page)
   (GET  "/about"                 about-page)
+  (GET  "/stylesheet/app.css"    stylesheet-page)
 
   (ANY "*"
     (page-not-found)))
