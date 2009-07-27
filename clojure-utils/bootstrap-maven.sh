3d7c5b26029e5c39906bc023ca1a49b45aa70bca
Kyle Burton <kyle.burton@gmail.com>
2009-07-26 20:53:23 -0400

    added pre-build of quartz adapter

--- clojure-utils/bootstrap-maven.sh
+++ clojure-utils/bootstrap-maven.sh
@@ -1,27 +1,27 @@
 set -e
 set -x
 
-#  test -d clojure || git clone git://github.com/richhickey/clojure.git
-#  cd clojure
-#  ant jar
-#  mvn install:install-file \
-#    -Dfile=clojure-1.1.0-alpha-SNAPSHOT.jar \
-#    -DgroupId=org.clojure \
-#    -DartifactId=clojure \
-#    -Dversion=1.1.0-alpha-SNAPSHOT \
-#    -Dpackaging=jar
-#  cd ../
-#  
-#  test -d clojure-contrib || git clone git://github.com/richhickey/clojure-contrib.git
-#  cd clojure-contrib
-#  ant jar
-#  mvn install:install-file \
-#    -Dfile=clojure-contrib.jar \
-#    -DgroupId=org.clojure \
-#    -DartifactId=clojure-contrib \
-#    -Dversion=1.1.0-alpha-SNAPSHOT \
-#    -Dpackaging=jar
-#  cd ..
+test -d clojure || git clone git://github.com/richhickey/clojure.git
+cd clojure
+ant jar
+mvn install:install-file \
+  -Dfile=clojure-1.1.0-alpha-SNAPSHOT.jar \
+  -DgroupId=org.clojure \
+  -DartifactId=clojure \
+  -Dversion=1.1.0-alpha-SNAPSHOT \
+  -Dpackaging=jar
+cd ../
+
+test -d clojure-contrib || git clone git://github.com/richhickey/clojure-contrib.git
+cd clojure-contrib
+ant jar
+mvn install:install-file \
+  -Dfile=clojure-contrib.jar \
+  -DgroupId=org.clojure \
+  -DartifactId=clojure-contrib \
+  -Dversion=1.1.0-alpha-SNAPSHOT \
+  -Dpackaging=jar
+cd ..
 
 test -f rabbitmq-java-client-1.6.0.tar.gz || wget http://www.rabbitmq.com/releases/rabbitmq-java-client/v1.6.0/rabbitmq-java-client-1.6.0.tar.gz
 test -d tar xzvf rabbitmq-java-client-1.6.0 || tar xzvf rabbitmq-java-client-1.6.0.tar.gz
@@ -33,4 +33,8 @@ mvn install:install-file \
   -DartifactId=amqp-client \
   -Dversion=1.6.1-SNAPSHOT \
   -Dpackaging=jar
+cd ..
+
+cd quartz-clojure-adapter
+mvn install
 cd ..
