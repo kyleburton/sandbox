991dc2468eac0859c60e07de3f122dbca0fb8782
Kyle Burton <kyle.burton@algorithmics.com>
2010-02-27 14:49:38 -0500

    more regex examples

--- examples/perl/strip-text/strip-text.pl
+++ examples/perl/strip-text/strip-text.pl
@@ -3,6 +3,13 @@ use warnings;
 
 while(<>) {
   chomp;
-  s|</example>\s*$||;
+  # strip the closing tag 'example' from lines, when it is at the end, NOT allowing for trailing spaces
+  #s|</example>$||i;
+  # strip the closing tag 'example' from lines, when it is at the end, allowing for trailing spaces
+  #s|</example>\s*$||i;
+  # strip the last closing XML tags on a line, allowing for trailing spaces, even if it's the only thing on the line
+  #s|</[^>]+>\s*$||i;
+  # strip the last closing XML tags on the line, allowing for trailing spaces, but only if there is some text before the closing tag
+  s|(?<=.)</[^>]+>\s*$||i;
   print $_,"\n"
 }
