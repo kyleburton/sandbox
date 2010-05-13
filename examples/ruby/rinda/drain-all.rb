6c466791d3f19d1f26a922043def444fa7c2f9f1
Kyle Burton <kburton@relaynetwork.com>
2010-05-13 15:53:26 -0400

    script for draining the queue

--- examples/ruby/rinda/drain-all.rb
+++ examples/ruby/rinda/drain-all.rb
@@ -11,7 +11,18 @@ $stderr.puts "obtaining Rinda::RingFinder.primary..."
 ring_server = Rinda::RingFinger.primary
 $stderr.puts "Rinda::RingFinder.primary obtained."
 
+tuple_timeout = 0
 [[nil],[nil,nil],[nil,nil,nil]].each do |tup|
-  tuples = ring_server.read_all tup
+  while true
+    items_found = false
+    begin
+      tup = ring_server.take tup, tuple_timeout
+      puts "popped: #{tup.inspect}"
+      items_found = true
+    rescue Rinda::RequestExpiredError => e
+      puts "timed out after #{tuple_timeout}, while waiting for :thing"
+    end
+    break unless items_found
+  end
 
 end
