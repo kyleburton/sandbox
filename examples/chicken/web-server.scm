d218dac0008b8576d12b210471296840ca418b1f
Kyle Burton <kyle.burton@gmail.com>
2012-01-22 11:14:54 -0500

    web server example

--- examples/chicken/web-server.scm
+++ examples/chicken/web-server.scm
@@ -1,10 +1,57 @@
 #!/usr/bin/env csi -q
-(use spiffy)
+;; See: http://wiki.call-cc.org/eggref/4/spiffy
+(use (prefix spiffy sp:) 
+     srfi-18  ;; threading
+     srfi-19) ;; time
 
-(server-port 8123)
-(root-path "./www")
-(define server (start-server))
+(sp:server-port 8123)
+(sp:root-path "./www")
 
+(define (datestamp)
+  (string-join
+    (list
+      (date->string (current-date) "~4")
+      (sprintf "~a" (current-milliseconds)))
+    "."))
+
+(define (handle-404 path)
+  (printf "in handle-404, current-request is: ~a\n" (sp:current-request))
+  (printf "in handle-404, current-response is: ~a\n" (sp:current-response))
+  (let [(msg (sprintf #<<END
+404: Not Found at ~a
+The path requested is: ~a
+
+END
+(datestamp)
+path))]
+    (print msg)
+   ;; NB: send-status is a wrapped version of send-response for common
+   ;; status codes, which wraps your the message with default html
+   ;;   (send-status 404 "Not Found" msg)
+   (sp:send-response
+     #:code 404
+     #:reason "Not Found"
+     #:body msg
+     #:headers `((content-type "text/plain")
+                 (content-length ,(string-length msg)))))) 
+
+(sp:handle-not-found handle-404)
+
+
+;;(define server (start-server))
+(define server #f)
+
+(define (start-server)
+  (set! server (make-thread 
+                 (lambda ()
+                   (sp:start-server))))
+  (thread-start! server))
+
+(define (stop-server)
+  (thread-terminate! server))
+
+(start-server)
 (printf "server: ~a\n" server)
+(printf "to exit / stop type (exit) and press enter\n")
 
-(exit)
+;; (exit)
