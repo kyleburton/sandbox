5b96922a6e22440efebfeffbb4c7e37ef27e0fbd
Kyle Burton <kburton@relaynetwork.com>
2012-02-04 16:10:38 -0500

    install rabbit

---
+++ dev-setup/linux/gui-install.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+sudo apt-get -y install gconf-editor compizconfig-settings-manager
+
--- dev-setup/linux/install.sh
+++ dev-setup/linux/install.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-sudo apt-get install ruby1.9.1 git emacs23-nox wget curl
+sudo apt-get -y install ruby1.9.1 git emacs23-nox wget curl rake
 
 cd
 if [ ! -d .profile.d ]; then
@@ -19,13 +19,13 @@ fi
 
 
 cd projects
-GITPROJS="sandbox teporingo clj-etl-utils system-utils krbemacs jrclj clj-bloom clj-xpath credit_card_validator impresario lein-marginalia lein-margauto perCEPtor dev-utils twilio-in-ten-minutes tellmewhen abstract-tables large-data-and-clojure base-app jbit clorine abottleinfrontofme kyleburton clj-lfsr fuzzy-string typrtail kyles-secret-interviewing-techniques intro-to-genetic-algorithms introduction-to-git"
+GITPROJS="sandbox teporingo clj-etl-utils system-utils krbemacs jrclj clj-bloom clj-xpath credit_card_validator impresario lein-marginalia lein-margauto perCEPtor dev-utils twilio-in-ten-minutes tellmewhen abstract-tables large-data-and-clojure base-app jbit clorine abottleinfrontofme clj-lfsr fuzzy-string typrtail kyles-secret-interviewing-techniques intro-to-genetic-algorithms introduction-to-git"
 
 for proj in $GITPROJS; do
-  git clone git@github.com:kyleburton/$proj.git
+  test -d $proj || git clone git@github.com:kyleburton/$proj.git
 done
 
-mkdir ~/projects/sandbox/dev-setup/linux/software
+test -d ~/projects/sandbox/dev-setup/linux/software || mkdir ~/projects/sandbox/dev-setup/linux/software
 cd ~/projects/sandbox/dev-setup/linux/software
 CHICKEN_VER=4.7.0
 
@@ -44,9 +44,23 @@ untar_file() {
 
 }
 
-download_file chicken-$CHICKEN_VER.tar.gz http://code.call-cc.org/releases/$CHICKEN_VER/chicken-$CHICKEN_VER.tar.gz
-untar_file chicken-$CHICKEN_VER chicken-$CHICKEN_VER.tar.gz
-cd chicken-$CHICKEN_VER
-make PLATFORM=linux
-make PLATFORM=linux PREFIX=$HOME/local/chicken-$CHICKEN_VER install
-ln -s $HOME/local/chicken-$CHICKEN_VER $HOME/local/chicken
+if [ ! -x $HOME/local/chicken/bin/csi ]; then
+    download_file chicken-$CHICKEN_VER.tar.gz http://code.call-cc.org/releases/$CHICKEN_VER/chicken-$CHICKEN_VER.tar.gz
+    untar_file chicken-$CHICKEN_VER chicken-$CHICKEN_VER.tar.gz
+    cd chicken-$CHICKEN_VER
+    make PLATFORM=linux
+    make PLATFORM=linux PREFIX=$HOME/local/chicken-$CHICKEN_VER install
+    ln -s $HOME/local/chicken-$CHICKEN_VER $HOME/local/chicken
+fi
+
+
+# rabbitmq
+if [ ! -f /etc/apt/sources.list.d/rabbitmq-sources.list ]; then
+    echo "deb http://www.rabbitmq.com/debian/ testing main" > rabbitmq-sources.list
+    sudo cp rabbitmq-sources.list /etc/apt/sources.list.d/
+    wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+    sudo apt-key add rabbitmq-signing-key-public.asc
+    sudo apt-get update
+fi
+
+sudo apt-get -y install rabbitmq-server
