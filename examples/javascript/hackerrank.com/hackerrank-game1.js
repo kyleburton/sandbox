2e83c7f82287360d019046093b5874c7190194ef
Kyle Burton <kyle.burton@gmail.com>
2012-07-01 23:33:07 -0400

    play all games

--- examples/javascript/hackerrank.com/hackerrank-game1.js
+++ examples/javascript/hackerrank.com/hackerrank-game1.js
@@ -4,7 +4,7 @@
 //   Foo.playGames( 7, 99 ) 
 // to play 99 games starting with the opening value of 7
 Foo = (function () {
-  var self = {autoContinue: false};
+  var self = {autoContinue: true, regressions: {}};
 
   self.startGame = function (numCandies) {
     $.ajax({
@@ -20,29 +20,49 @@ Foo = (function () {
   };
 
   self.nextMove = function (current) {
-    return current % 6;
+    var move = current % 6;
+    if ( move === 0 ) {
+      return 1;
+    }
+    return move;
   };
 
   self.playRound = function (data) {
-    var nextMove;
+    var nextMove, gameOver = false;
     console.log('play round: ' + JSON.stringify(data));
     if (data.message) {
       console.log(data.message);
     }
 
-    //if (data.exit) {
+    if (data.exit) {
+      gameOver = true;
     //  console.log('Game is over!');
     //  return;
-    //}
+    }
 
     if (data.game) {
       data = data.game;
     }
 
     if (data.solved) {
-      console.log('Game is solved!');
+      console.log('Won the game :)');
       self.continue = null;
-      return self.gameWon();
+      return self.gameFinished();
+    }
+
+    if (gameOver) {
+      console.log('Lost the game :(');
+      self.continue = null;
+      return self.gameFinished();
+    }
+
+    if (data.n !== self.currentValue) {
+      console.error('ERROR: regression, data.n=%s self.currentValue=%s', data.n, self.currentValue);
+      self.regressions[self.currentValue] = self.regressions[self.currentValue] || [];
+      self.regressions[self.currentValue].push(data.n);
+      // skip the game if it regresses
+      self.playGames(self.currentValue-1);
+      return;
     }
 
     // n the starting numCandies
@@ -50,7 +70,7 @@ Foo = (function () {
     // limit: not sure wha thtis is
     // moves array of moves taken?
     nextMove = self.nextMove(data.current);
-    console.log('the next move is: ' + nextMove);
+    // console.log('the next move is: ' + nextMove);
     if (nextMove < 1 ) {
       console.error('Whoops something went wrong, the next move is zero? ');
       console.dir(data);
@@ -77,24 +97,24 @@ Foo = (function () {
     }
   };
 
-  self.playGames = function ( startingValue, numGames ) {
-    self.numGames = numGames;
-
-    self.currentValue = startingValue - (startingValue % 6) + 1,
-
-    self.gameWon = function () {
-      if (self.numGames < 1) {
-        console.log('games played, currentValue: ' + self.currentValue);
+  self.playGames = function (startValue) {
+    self.currentValue = 6;
+    if (startValue) {
+      self.currentValue = startValue;
+    }
+    self.gameFinished = function () {
+      if (self.currentValue >= 2560) {
+        console.log('all games played');
         return;
       }
-      self.numGames = self.numGames - 1;
-      self.currentValue += 6;
-      console.log('playing next game[' + self.numGames + ']: ' + self.currentValue);
+      if (self.currentValue < 1) {
+        console.log('all games played');
+        return;
+      }
+      //self.currentValue = self.currentValue + 1;
+      self.currentValue = self.currentValue - 1;
       return self.startGame(self.currentValue);
-    };
-
-    self.numGames = self.numGames - 1;
-    console.log('playing next game[' + self.numGames + ']: ' + self.currentValue);
+    }
     return self.startGame(self.currentValue);
   };
 
