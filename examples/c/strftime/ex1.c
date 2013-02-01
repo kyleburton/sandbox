537ad776d209e0f1dcadb73686a8b3242739559f
Kyle Burton <kyle.burton@gmail.com>
2013-01-31 23:45:17 -0500

    strftime example

--- examples/c/strftime/ex1.c
+++ examples/c/strftime/ex1.c
@@ -5,11 +5,36 @@
 
 int main (const int argc, const char ** argv ) {
   time_t curr_time;
+  char buff[1024], daynamebuff[8], monbuff[8], daynumbuff[3], yearbuff[8];
+
+  // time(&curr_time);
+  curr_time = 1359684105; // Thu Jan 31 2013
+  curr_time += 24 * 60 * 60; // Fri Feb 01 2013
+  struct tm *now = localtime(&curr_time);
+
+  strftime(daynamebuff, sizeof(daynamebuff), "%a", now);
+  strftime(monbuff,     sizeof(monbuff), "%b", now);
+  strftime(daynumbuff,  sizeof(daynumbuff), "%e", now);
+  strftime(yearbuff,    sizeof(yearbuff), "%Y", now);
+
+  sprintf(buff, "%s %s %02d %s", daynamebuff, monbuff, now->tm_mday, yearbuff);
+  printf("%s\n", buff);
+  return 0;
+}
+
+int main2 (const int argc, const char ** argv ) {
+  time_t curr_time;
   char buff[1024];
+
   // time(&curr_time);
   curr_time = 1359684105; // Thu Jan 31 2013
   struct tm *now = localtime(&curr_time);
+
   strftime(buff, sizeof(buff),  "%a %b %d %Y", now);
+  printf("time: %ld\n", curr_time);
+  printf("time: %s\n", buff);
+
+  strftime(buff, sizeof(buff),  "%a %b %e %Y", now);
   printf("time: %ld\n", curr_time);
   printf("time: %s\n", buff);
 
@@ -18,5 +43,10 @@ int main (const int argc, const char ** argv ) {
   strftime(buff, sizeof(buff),  "%a %b %d %Y", now);
   printf("time: %ld\n", curr_time);
   printf("time: %s\n", buff);
+
+  strftime(buff, sizeof(buff),  "%a %b %e %Y", now);
+  printf("time: %ld\n", curr_time);
+  printf("time: %s\n", buff);
   return 0;
 }
+
