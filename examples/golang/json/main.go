d845ecaba240b1d435ba37fa968dca421b771cf1
Kyle Burton <kyle.burton@gmail.com>
2013-07-14 10:57:51 -0400

    add String for message struct, add more json to the second example

--- examples/golang/json/main.go
+++ examples/golang/json/main.go
@@ -7,15 +7,20 @@ import (
 	"io"
 )
 
+type Message struct {
+  Field1 string
+  Field2 []float64
+}
+
+func (self *Message) String() string {
+  return fmt.Sprintf("{\"Field1\": %s, \"Field2\": %q}", self.Field1, self.Field2)
+}
+
+
 func main() {
   const jsonStr = `
 {"Field1": "that", "Field2": [1, 3, 3.4]}
 `
-
-  type Message struct {
-    Field1 string
-    Field2 []float64
-  }
 
   dec := json.NewDecoder(strings.NewReader(jsonStr))
   for {
@@ -25,11 +30,11 @@ func main() {
     } else if err != nil {
       panic(err)
     }
-    fmt.Printf("Decoded: %q\n", m)
+    fmt.Printf("Decoded: %q\n", &m)
   }
 
   const jsonStr2 = `
-{"this": "that", "other": [1, 3, 3.4], "nested": {"thing": ["five", 3]}}
+{"this": "that", "other": [1, 3, 3.4], "nested": {"thing": ["five", 3]}, "moreStuff": {"andMore": [{"a": 1, "b": {"c": 3}}]}}
 `
   stuff := make(map[string] interface{})
 
