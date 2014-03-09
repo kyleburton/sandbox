38009d31372f4e988845b2a7aa7c04be0cc0523a
Kyle Burton <kburton@relaynetwork.com>
2014-03-09 11:08:32 -0400

    scratch

---
+++ examples/javascript/linkify-urls/linkify.js
@@ -0,0 +1,10 @@
+(function (root) {
+  var self = {};
+  root.Linkify = self;
+
+
+  self.findLinksInElement = function (elt) {
+    console.log('findLinksInElement');
+    return [];
+  };
+}(window));
