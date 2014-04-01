51f708751a61dd445b8fe0709fb59dbf6f5017f5
Kyle Burton <kyle.burton@gmail.com>
2014-03-31 22:28:38 -0400

    notes

--- examples/digitalocean.com/test.sh
+++ examples/digitalocean.com/test.sh
@@ -1,3 +1,7 @@
 go run pkg/diocean/main.go droplets new test1 512mb ubuntu-13-10-x64 nyc2 20848 false false
+# grab the event and the droplet_id
 go run pkg/diocean/main.go droplets ls
+# wait for the droplet to spin up
+echo go run pkg/diocean/main.go events wait 12345
+# destroy teh droplet
 echo go run pkg/diocean/main.go droplets destroy 1390159 false
