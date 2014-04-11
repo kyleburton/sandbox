6609ced9e9f98bf1bdcd4c74f87a94377f4cb35a
Kyle Burton <kyle.burton@gmail.com>
2014-04-11 15:26:57 -0400

    skeleton

---
+++ examples/leiningen/quick-rest/project.clj
@@ -0,0 +1,10 @@
+(defproject quick-rest "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [swank-clojure                               "1.4.3"]
+                 [com.relaynetwork/kinematic                  "1.3.6"]
+                 [com.relaynetwork/clorine                    "1.0.17"]
+                 [http-kit                                    "2.1.16"]])
---
+++ examples/leiningen/quick-rest/test/quick_rest/core_test.clj
@@ -0,0 +1,7 @@
+(ns quick-rest.core-test
+  (:use clojure.test
+        quick-rest.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
