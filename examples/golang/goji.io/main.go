7c66fac63e7a0df8d1d06c20cc97b0bcdffdb13d
Kyle Burton <kyle.burton@gmail.com>
2014-04-23 08:18:21 -0400

    goji example

---
+++ examples/golang/goji.io/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+        "fmt"
+        "net/http"
+
+        "github.com/zenazn/goji"
+        "github.com/zenazn/goji/web"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+}
+
+func main() {
+        goji.Get("/hello/:name", hello)
+        goji.Serve()
+}
