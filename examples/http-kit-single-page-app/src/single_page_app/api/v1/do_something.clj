c449be33bf7439fc62a48c4871ba01604a21e04d
Kyle Burton <kyle.burton@gmail.com>
2014-09-22 09:44:28 -0400

    example

--- examples/http-kit-single-page-app/src/single_page_app/api/v1/do_something.clj
+++ examples/http-kit-single-page-app/src/single_page_app/api/v1/do_something.clj
@@ -5,28 +5,41 @@
 
 (defonce stats (atom {}))
 
+(defn response-body [m]
+  (let [resp-time (java.util.Date.)]
+    (->
+     m
+     (assoc
+         :stats     @stats
+         :time      (str resp-time)
+         :time_ms   (.getTime resp-time)
+         :time_nano (System/nanoTime))
+     json/json-str)))
+
 (defn get-handler [request]
+  (def request request)
   (swap! stats update-in [:requests :get] #(inc (or % 0)))
   (swap! stats update-in [:requests :all] #(inc (or % 0)))
-  {:status  200
-   :headers {"Content-type" "application/json"}
-   :body    (json/json-str {:params (-> request :params)
-                            :time   (str (java.util.Date.))
-                            :name   (-> request :session :name)
-                            :stats  @stats})
-   :session (-> request :session)})
+  (let [num1 (-> request :query-params (get "num1") Integer/parseInt)
+        num2 (-> request :query-params (get "num2") Integer/parseInt)]
+   {:status  200
+    :headers {"Content-type" "application/json"}
+    :body    (response-body {:params (-> request :params)
+                             :name   (-> request :session :name)
+                             :answer (+ num1 num2)})
+    :session (-> request :session)}))
 
 (defn post-handler [request]
   (swap! stats update-in [:requests :post] #(inc (or % 0)))
-  (let [user-name (or
+  (let [body (slurp (:body request))
+        _ (def body body)
+        user-name (or
                    (-> request :params :name)
                    (-> request :session :name))]
     {:status  200
      :headers {"Content-type" "application/json"}
-     :body    (json/json-str {:params (-> request :params)
-                              :time   (str (java.util.Date.))
-                              :name   user-name
-                              :stats  @stats})
+     :body    (response-body {:params (-> request :params)
+                              :name   (-> request :session :name)})
      :session (->
                request
                :session
