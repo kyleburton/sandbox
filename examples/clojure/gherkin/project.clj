a5dea402f2c0491ba3f79996eba9b7b2076b2f89
Kyle Burton <kyle.burton@gmail.com>
2015-01-18 17:27:12 +0000

    wip

---
+++ examples/clojure/gherkin/project.clj
@@ -0,0 +1,18 @@
+(defproject gherkin "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main ^:skip-aot silent-auction-services.core
+  :jvm-opts ["-Xmx512m" "-server" "-Dorg.quartz.scheduler.skipUpdateCheck=true"]
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging              "0.3.0"]
+                 [org.clojure/tools.nrepl                "0.2.3"]
+                 [cider/cider-nrepl                      "0.7.0"]
+                 [org.clojure/data.json                  "0.2.5"]
+                 [prismatic/schema                       "0.3.1"]
+                 [clj-time                               "0.8.0"]
+                 [org.clojure/core.cache                 "0.6.4"]
+                 [org.clojure/data.csv                   "0.1.2"]
+                 ])
