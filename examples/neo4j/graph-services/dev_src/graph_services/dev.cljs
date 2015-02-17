48ae22a0753559c35c23dc073bea8d86be4c37df
Kyle Burton <kyle.burton@gmail.com>
2015-02-17 09:13:43 -0500

    working on install/setup

---
+++ examples/neo4j/graph-services/dev_src/graph_services/dev.cljs
@@ -0,0 +1,10 @@
+(ns graph-services.dev
+    (:require
+     [graph-services.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
