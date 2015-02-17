1f3bd58c161c69860e1393b3a527fa504ced577c
Kyle Burton <kyle.burton@gmail.com>
2015-02-17 11:29:15 -0500

    working on installers for redis/rabbitmq

---
+++ examples/rabbitmq/config/env.sh
@@ -0,0 +1,4 @@
+RABBITMQ_3_1_2_PACKAGE_URL="http://www.rabbitmq.com/releases/rabbitmq-server/v3.1.2/rabbitmq-server-generic-unix-3.1.2.tar.gz"
+RABBITMQ_3_4_4_PACKAGE_URL="http://www.rabbitmq.com/releases/rabbitmq-server/v3.4.4/rabbitmq-server-generic-unix-3.4.4.tar.gz"
+
+REDIS_2_8_19_PACKAGE_URL="http://download.redis.io/releases/redis-2.8.19.tar.gz"
