8d2f7fd86d60992b42f46007590ad655ff7c5892
Kyle Burton <kyle.burton@gmail.com>
2015-10-26 12:03:17 -0400

    ex: iteration

--- examples/java/clj-ds/src/main/java/Main.java
+++ examples/java/clj-ds/src/main/java/Main.java
@@ -1,14 +1,18 @@
-
 import com.github.krukow.clj_lang.IPersistentMap;
 import com.github.krukow.clj_lang.PersistentHashMap;
+import java.util.Map;
 
 public class Main {
   public static void main (String [] args) {
-    IPersistentMap m = PersistentHashMap.emptyMap();
+    IPersistentMap<Object,Object> m = PersistentHashMap.emptyMap();
     System.out.println("map=" + m);
     m = m.assoc("this","that");
     System.out.println("map=" + m);
     m = m.assoc("other",42);
     System.out.println("map=" + m);
+
+    for (Map.Entry<Object,Object> e : (PersistentHashMap<Object,Object>) m) {
+      System.out.println("k[" + e.getKey() + "]=v[" + e.getValue() + "]");
+    }
   }
 }
