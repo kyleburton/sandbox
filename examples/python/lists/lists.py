98c621bbaf27e5565e8524bb1a9ea31b43a36ca8
Kyle Burton <kyle.burton@gmail.com>
2015-11-15 08:02:28 -0800

    list examples with slicing

---
+++ examples/python/lists/lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+
+
+s = "abracadbra"
+
+print "slice[-1] %s" % (s[-1])
+
+print "slice[-2:] %s" % (s[-2:])
+print "slice[:] %s" % (s[:])
+print "slice[0:-1] %s" % (s[0:-1])
+print "slice[::1] %s" % (s[::1])
+print "slice[::2] %s" % (s[::2])
+print "slice[::3] %s" % (s[::3])
+print "slice[::4] %s" % (s[::4])
+print "slice[::5] %s" % (s[::5])
+for ii in range(1, 2 * len(s)):
+  print "slice[::%s] %s" % (ii, s[::ii])
