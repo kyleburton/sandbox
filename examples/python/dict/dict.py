b7e2034cd7b1405953ba907be4e8f0f2bb7b48d5
Kyle Burton <kyle.burton@gmail.com>
2015-11-21 10:57:38 -0800

    additional dict examples

--- examples/python/dict/dict.py
+++ examples/python/dict/dict.py
@@ -2,23 +2,34 @@ d = {"a": 1, "b": 2}
 
 d["c"] = 3
 
-print "for k in d"
+print("for k in d")
 for k in d:
   v = d[k]
-  print "%s => %s" % (k,v)
+  print("%s => %s" % (k,v))
 print
 
-print "for k,v in d.iteritems()"
-for k, v in d.iteritems():
-  print "%s => %s" % (k,v)
-print
+try:
+  print("for k,v in d.iteritems()")
+  for k, v in d.iteritems():
+    print("%s => %s" % (k,v))
+  print
+except:
+  print("dict.iteritems is supported in python2, but not python3")
 
-print "for k,v in d.items()"
+print("for k,v in d.items()")
 for k, v in d.items():
-  print "%s => %s" % (k,v)
+  print("%s => %s" % (k,v))
 print
 
 
-print "iter sorted"
+print("iter sorted")
 for k in sorted(d):
-  print "%s => %s" % (k,d[k])
+  print("%s => %s" % (k,d[k]))
+
+try:
+  print("d['missing']={}".format(d["missing"]))
+except:
+  print("can't access a key that's not there")
+
+
+print("d.get('missing')={}".format(d.get("missing")))
