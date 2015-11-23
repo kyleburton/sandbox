391a187d7e13fd5c4c3c32ac68acc74197c84a60
Kyle Burton <kyle.burton@gmail.com>
2015-11-22 16:43:57 -0800

    python examples

--- examples/python/extensions/test/test_spam.py
+++ examples/python/extensions/test/test_spam.py
@@ -2,9 +2,14 @@ import unittest
 import doctest
 
 class SpamTest(unittest.TestCase):
-  def runTest(self):
+  def testImport(self):
     try:
       import spam
     except ImportError, e:
       self.Fail(str(e))
 
+  def testSystem(self):
+    import spam
+    res = spam.system("ls -l")
+    if not res:
+      self.Fail("system returned falsy value: %s" % (res))
