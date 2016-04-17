bfe99fc6d747a83f91af52287ada79cbee167daf
Kyle Burton <kyle.burton@gmail.com>
2016-04-17 13:05:31 -0400

    moved out to its own project

--- examples/clojure/repl-from-java/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to repl-from-java
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- examples/clojure/repl-from-java/test/repl_from_java/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns repl-from-java.core-test
-  (:require [clojure.test :refer :all]
-            [repl-from-java.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
