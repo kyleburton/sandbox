96d4c531632dadfd748cc4ba43a3a824e4c9308b
Kyle Burton <kyle.burton@gmail.com>
2016-04-23 08:46:40 -0700

    little shadowing example

--- examples/rust/book/hello_world2/src/main.rs
+++ examples/rust/book/hello_world2/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world2!");
+    let x = 32;
+    println!("Hello, world2! x={}", x);
+    // NB: it looks like when you shadow, you can change a variable's type
+    let x = "three";
+    println!("Hello, world2! x={}", x);
 }
