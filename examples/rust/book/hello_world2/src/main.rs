8a0f621a54ddd6867492d5dc5d4883d0ebbb0b12
Kyle Burton <kyle.burton@gmail.com>
2016-04-23 08:48:18 -0700

    updated

--- examples/rust/book/hello_world2/src/main.rs
+++ examples/rust/book/hello_world2/src/main.rs
@@ -4,4 +4,12 @@ fn main() {
     // NB: it looks like when you shadow, you can change a variable's type
     let x = "three";
     println!("Hello, world2! x={}", x);
+
+    let mut x = 32;
+    println!("Hello, world2! x={}", x);
+    x = 16;
+    println!("Hello, world2! x={}", x);
+    // NB: x is now immutable!
+    let x = x + -8;
+    println!("Hello, world2! x={}", x);
 }
