4eddd1baed10647d859a46a85c148c7155bc5227
Kyle Burton <kyle.burton@gmail.com>
2016-05-15 09:57:13 -0700

    example

---
+++ examples/clojure/pedestal-swagger/project.clj
@@ -0,0 +1,30 @@
+(defproject pedestal-swagger "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+
+                 ;; Remove this line and uncomment one of the next lines to
+                 ;; use Immutant or Tomcat instead of Jetty:
+                 [io.pedestal/pedestal.jetty "0.4.1"]
+                 ;; [io.pedestal/pedestal.immutant "0.4.1"]
+                 ;; [io.pedestal/pedestal.tomcat "0.4.1"]
+
+                 [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.12"]
+                 [org.slf4j/jcl-over-slf4j "1.7.12"]
+                 [org.slf4j/log4j-over-slf4j "1.7.12"]
+
+                 [prismatic/schema                       "1.1.1"]
+                 [org.clojure/tools.nrepl                "0.2.12"]
+                 [cider/cider-nrepl                      "0.10.2"]
+                ]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "pedestal-swagger.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.4.1"]]}
+             :uberjar {:aot [pedestal-swagger.server]}}
+  :main ^{:skip-aot true} pedestal-swagger.server)
+
