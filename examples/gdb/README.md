127dd1ca9d3285c57b0b3b9a3203641cc57f8d56
Kyle Burton <kyle.burton@gmail.com>
2016-07-15 16:36:35 -0700

    example

---
+++ examples/gdb/README.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=PorfLSr3DDI
+
+CTRL-X a
---
+++ examples/gdb/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void) {
+  int i = 0;
+  printf("Hello, world\n");
+  printf("i is %d\n", i);
+  i++;
+  printf("i is now %d\n", i);
+  return 0;
+}
