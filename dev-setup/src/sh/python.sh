714bca6df80c32a759e96534eb6886b75766d156
Kyle Burton <kyle.burton@gmail.com>
2017-02-21 07:36:00 -0800

    ubuntu: common pre-req libs: bz2, readline, sqlite3; install pythons

--- dev-setup/src/sh/python.sh
+++ dev-setup/src/sh/python.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+PYENV_VERSIONS="2.7.13 3.6.0"
+
+bake_task python:install "use pyenv to install python versions: $PYENV_VERSIONS"
 function python:install () {
-  pyenv install --skip-existing 3.6.0
+  for ver in $PYENV_VERSIONS; do
+    pyenv install --skip-existing "$ver"
+  done
 }
