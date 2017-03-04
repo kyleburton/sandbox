ff69d3e7be5f48930034a5fce8df13f42a936802
Kyle Burton <kyle.burton@gmail.com>
2017-03-04 11:53:12 -0800

    handle brew packages that are already installed

--- dev-setup/src/sh/linuxbrew.sh
+++ dev-setup/src/sh/linuxbrew.sh
@@ -2,11 +2,17 @@
 
 function linuxbrew:install-packages () {
   os-utils:cat-cleanly "$pkg_file" | while read -r line; do
-    echo "brew install $line"
-    $HOME/.linuxbrew/bin/brew install $line
+    local pkg_name="$(echo "$line" | cut -f1 -d ' ')"
+    if brew ls --versions "$pkg_name"; then
+      bake_echo_green "OK: brew $pkg_name already installed ($line)"
+      continue
+    fi
+    bake_echo_blue "brew install $pkg_name via $line"
+    $HOME/.linuxbrew/bin/brew install $line || echo "Mayb ok if it's already installed? $line"
   done
 }
 
+bake_task linuxbrew:install "Install packages via brew"
 function linuxbrew:install () {
   local pkg_file="${1:-brew.packages}"
   if [ ! -d "$HOME/.linuxbrew" ]; then
