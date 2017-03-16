91ba76d94fd9e6cf753e08339d420273b664bf5e
Kyle Burton <kyle.burton@gmail.com>
2017-03-15 22:31:09 -0700

    add counter component

--- examples/react.js/hello-react/src/main.js
+++ examples/react.js/hello-react/src/main.js
@@ -1,1 +1,11 @@
 console.log("Hello, World!")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+      React.createElement(Counter),
+      document.getElementById('mount')
+      );
+});
