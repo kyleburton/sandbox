4557becd85300deab3caf9c41f4d93dd702f036f
Kyle Burton <kyle.burton@gmail.com>
2017-05-13 18:45:35 -0700

    enable reloading

--- examples/python/flask-in-docker/.env
+++ examples/python/flask-in-docker/.env
@@ -1,1 +1,2 @@
 FLASK_APP=app.py
+FLASK_DEBUG=1
--- examples/python/flask-in-docker/web/app.py
+++ examples/python/flask-in-docker/web/app.py
@@ -11,9 +11,16 @@ app = Flask(__name__)
 
 # from models import *
 
+
 @app.route('/hello')
-def submit():
+def hello_route():
     return 'hello, this is a flask app!'
+
+
+# uncommenting this after the app is running will cause it to be reloaded
+# @app.route('/goodbye')
+# def goodbye_route():
+#     return 'bye! this was a flask app!'
 
 if __name__ == '__main__':
     app.run()
