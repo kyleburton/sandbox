1f8997f7170faa7ddb14528e3d1903fa9cd47d7d
Kyle Burton <kyle.burton@gmail.com>
2017-06-04 14:29:29 -0700

    wip

--- examples/rust/programming-rust/strings/src/main.rs
+++ examples/rust/programming-rust/strings/src/main.rs
@@ -71,4 +71,31 @@ fn main() {
         println!("l2={:?}", l2);
         // nb: b/c point leaves scope here, it gets freed
     }
+
+    {
+        struct Person {
+            name: String,
+            birth: i32,
+        }
+
+        let mut composers = Vec::new();
+
+        composers.push(Person {
+            name: "Palestrina".to_string(),
+            birth: 1525,
+        });
+        composers.push(Person {
+            name: "Dowland".to_string(),
+            birth: 1563,
+        });
+        composers.push(Person {
+            name: "Lully".to_string(),
+            birth: 1632,
+        });
+
+        for composer in &composers {
+            println!("{}, born: {}", composer.name, composer.birth);
+        }
+
+    }
 }
