04644918605c5e46ba0374057d484e53a2402e52
Kyle Burton <kyle.burton@gmail.com>
2017-07-16 11:44:37 -0700

    vec example, with operator overloads

---
+++ examples/python/fluent-python/README.md
@@ -0,0 +1,3 @@
+Exercises & Scratch Code from the O'Reilly Title Fluent Python
+
+  http://shop.oreilly.com/product/0636920032519.do
---
+++ examples/python/fluent-python/ch1.py
@@ -0,0 +1,22 @@
+from math import hypot
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "Vector({}, {})".format(self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
