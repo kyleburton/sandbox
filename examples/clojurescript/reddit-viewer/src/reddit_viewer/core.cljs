6181ac6826570a2d67d045f090cf8ca70622cd36
Kyle Burton <kyle.burton@gmail.com>
2017-07-31 07:52:24 -0700

    show chart example, a non-react js library wrapped in the lifecycle of a react component

--- examples/clojurescript/reddit-viewer/src/reddit_viewer/core.cljs
+++ examples/clojurescript/reddit-viewer/src/reddit_viewer/core.cljs
@@ -1,7 +1,8 @@
 (ns reddit-viewer.core
   (:require
-   [ajax.core :as ajax]
-   [reagent.core :as r]))
+   [ajax.core           :as ajax]
+   [reagent.core        :as r]
+   [reddit-viewer.chart :as chart]))
 
 ;; -------------------------
 ;; our data
@@ -30,6 +31,13 @@
 ;; -------------------------
 ;; Views
 
+(defn sort-posts [title sort-key]
+  (when-not (empty? @posts)
+    [:button.btn.btn-secondary
+     ;; {:on-click #(swap! posts (partial sort-by sort-key))}
+     {:on-click #(swap! posts (fn [posts] (reverse (sort-by sort-key posts))))}
+     (str "sort posts by " title)]))
+
 (defn display-post [{:keys [permalink subreddit title score url]}]
   [:div.card.m-2
    [:div.card-block
@@ -38,9 +46,23 @@
     [:div [:span.badge.badge-info {:color "info"} subreddit " score " score]]
     [:img {:width "300px" :src url}]]])
 
+(defn display-posts [posts]
+  (when-not (empty? posts)
+    [:div
+     (for [posts-row (partition-all 3 posts)]
+       ^{:key posts-row}
+       [:div.row
+        (for [post posts-row]
+          ^{:key post}
+          [:div.col-4 [display-post post]])])]))
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]
-   [display-post (first @posts)]])
+  [:div.card>div.card-block
+   [:div.btn-group
+    [sort-posts "score" :score]
+    [sort-posts "comments" :num_comments]]
+   [chart/chart-posts-by-votes posts]
+   [display-posts @posts]])
 
 ;; -------------------------
 ;; Initialize app
