674156d8525d3e37c658274a26ec7c2c05de8cd1
Kyle Burton <kyle.burton@gmail.com>
2017-07-31 07:42:48 -0700

    ajax and posts

--- examples/clojurescript/reddit-viewer/src/reddit_viewer/core.cljs
+++ examples/clojurescript/reddit-viewer/src/reddit_viewer/core.cljs
@@ -1,12 +1,46 @@
 (ns reddit-viewer.core
-    (:require
-      [reagent.core :as r]))
+  (:require
+   [ajax.core :as ajax]
+   [reagent.core :as r]))
+
+;; -------------------------
+;; our data
+(defonce posts (r/atom nil))
+(defonce last-request (r/atom nil))
+
+;; from the Chrome javascript console, you can view the atoms by
+;; calling the cljs functions directly:
+;; 
+;; cljs.core.clj__GT_js(cljs.core.deref(reddit_viewer.core.last_request))
+
+(defn find-posts-with-preview [posts]
+  (filter #(= (:post_hint %) "image") posts))
+
+(defn load-posts []
+  (ajax/GET "https://www.reddit.com/r/Catloaf.json?sort=new&limit=10"
+            {:handler         #(do
+                                 (reset! last-request %)
+                                 (->> (get-in % [:data :children])
+                                      (map :data)
+                                      (find-posts-with-preview)
+                                      (reset! posts)))
+             :response-format :json
+             :keywords?       true}))
 
 ;; -------------------------
 ;; Views
 
+(defn display-post [{:keys [permalink subreddit title score url]}]
+  [:div.card.m-2
+   [:div.card-block
+    [:h4.card-title
+     [:a {:href (str "http://reddit.com" permalink)} title " "]]
+    [:div [:span.badge.badge-info {:color "info"} subreddit " score " score]]
+    [:img {:width "300px" :src url}]]])
+
 (defn home-page []
-  [:div [:h2 "Welcome to Reagent"]])
+  [:div [:h2 "Welcome to Reagent"]
+   [display-post (first @posts)]])
 
 ;; -------------------------
 ;; Initialize app
