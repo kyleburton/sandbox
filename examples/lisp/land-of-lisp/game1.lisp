050b4ea0aa1c5fa6679fd5bb3bf21cddde8c1db2
Kyle Burton <kyle.burton@gmail.com>
2017-11-04 11:06:04 -0700

    starting on the book

---
+++ examples/lisp/land-of-lisp/game1.lisp
@@ -0,0 +1,14 @@
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
