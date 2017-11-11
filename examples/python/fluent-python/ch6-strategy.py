f47163cc225654e2340490f63cca016b633af86c
Kyle Burton <kyle.burton@gmail.com>
2017-11-11 12:12:43 -0800

    fluent python examples

--- examples/python/fluent-python/ch6-strategy.py
+++ examples/python/fluent-python/ch6-strategy.py
@@ -1,46 +1,6 @@
+from store import Customer, LineItem, Order
 from abc import ABC, abstractmethod
 from collections import namedtuple
-
-
-Customer = namedtuple('Customer', 'name fidelity')
-
-
-class LineItem:
-    def __init__(self, product, quantity, price):
-        self.product = product
-        self.quantity = quantity
-        self.price = price
-
-    def total(self):
-        return self.quantity * self.price
-
-
-class Order:  # the Context
-    def __init__(self, customer, cart, promotion=None):
-        self.customer = customer
-        self.cart = list(cart)
-        self.promotion = promotion
-
-    def total(self):
-        return sum(item.total() for item in self.cart)
-
-    def due(self):
-        return self.total() - self.discount()
-
-    def discount(self):
-        if self.promotion is None:
-            return 0
-        if callable(self.promotion):
-            return self.promotion(self)
-        return self.promotion.discount(self)
-
-    def __repr__(self):
-        fmt = '<Order total: {:.2f} due: {:.2f} promo:{} discount: {:.2f}>'
-        return fmt.format(
-            self.total(),
-            self.due(),
-            self.promotion.__class__.__name__,
-            self.discount())
 
 
 class Promotion(ABC):  # the Strategy: an abstract base class
--- examples/python/fluent-python/ch7-decorators.py
+++ examples/python/fluent-python/ch7-decorators.py
@@ -1,3 +1,6 @@
+from store import Customer, LineItem, Order
+
+
 def decorate(f):
     f.__cache = []
 
@@ -46,12 +49,63 @@ def f3():
     print('in f3()')
 
 
-def main():
+promotions = []
+
+
+def promotion(promo_func):
+    promotions.append(promo_func)
+    return promo_func
+
+@promotion
+def fidelity_promo(order):
+    """5% discount for customers with 1000 or more fidelity points"""
+    return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+@promotion
+def bulk_item(order):
+    """10% discount for each line item with 20 or more units"""
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * 0.1
+    return discount
+
+@promotion
+def large_order(order):
+    """7% discount for orders with 10 or more distinct items"""
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items) >= 10:
+        return order.total() * 0.07
+    return 0
+
+def best_promo(order):
+    """Select the best promotion (discount) available for the order"""
+    return max(promo(order) for promo in promotions)
+    
+
+
+
+def main2():
     print("in main()")
     print("registry: {}".format(registry))
     f1()
     f2()
     f3()
+
+def main():
+    jon = Customer("John Doe", 0)
+    ann = Customer("Ann Smith", 1100)
+    cart = [
+        LineItem('banana',        4,  0.5),
+        LineItem('apple',        10,  1.5),
+        LineItem('watermellon',   5,  5.0),
+    ]
+    o = Order(jon, cart, fidelity_promo)
+    o2 = Order(ann, cart, fidelity_promo)
+    print("o:  {}".format(o))
+    print("o2: {}".format(o2))
+    print("best_promo(o):  {}".format(best_promo(o)))
+    print("best_promo(o2): {}".format(best_promo(o2)))
 
 
 if __name__ == '__main__':
