eb5c0309df1b16f8db8ca92f08023efbc1753884
Kyle Burton <kyle.burton@gmail.com>
2017-11-29 14:17:19 -0800

    playing with the .? operator

--- examples/kotlin/hello/hello.kt
+++ examples/kotlin/hello/hello.kt
@@ -4,4 +4,32 @@ fun main(args : Array<String>) {
 	for(arg in args) {
 		println("arg: $arg")
 	}
+
+	var ii : String?
+	ii = null
+	doSomething(ii)
+	ii = ""
+	doSomething(ii)
+	ii = "whoa hey nice"
+	doSomething(ii)
+}
+
+
+fun doSomething(s : String?) {
+	println("doSomething: this is before all the coming if nonsense")
+	if (s?.length == 0) {
+		println("doSomething: er, uh that's an empty string there.")
+	}
+	else {
+		// println("doSomething: hey, you've got a length of: " + s.length)
+		println("doSomething: hey, you've got a length of: " + s?.length)
+		//println("doSomething: hey, you've got a length of: " + s!!.length)
+	}
+
+	if (s?.length == null) {
+		println("doSomething: haha, it's nulll ;'-)")
+	}
+
+	println("doSomething: this is after all that if nonsense")
+	println("")
 }
