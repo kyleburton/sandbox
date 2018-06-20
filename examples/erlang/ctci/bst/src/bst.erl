bc33e08fe6ed33ba76e084b0b57ba1e4fbd7c04e
Kyle Burton <kyle.burton@gmail.com>
2018-06-19 19:27:25 -0700

    wip

--- examples/erlang/ctci/bst/src/bst.erl
+++ examples/erlang/ctci/bst/src/bst.erl
@@ -1,6 +1,51 @@
 -module(bst).
 
--export([bst_new/0]).
+-export([new/1,
+         length/1,
+         add/2,
+         pre_order_traverse/3]).
 
-bst_new() ->
-    none.
+new(CmpFn) ->
+    {bst, CmpFn, none}.
+
+length(T) ->
+    pre_order_traverse(
+      T,
+      fun (_Val, Acc) ->
+              Acc + 1
+      end,
+      0).
+
+pre_order_traverse({bst, _CmpFn, Top}, F, Acc) ->
+    pre_order_traverse2(Top, F, Acc).
+
+pre_order_traverse2(none, _F, Acc) ->
+    Acc;
+pre_order_traverse2({none, Val, none}, F, Acc) ->
+    F(Val, Acc);
+pre_order_traverse2({Left, Val, none}, F, Acc) ->
+    Acc2 = pre_order_traverse2(Left, F, Acc),
+    F(Val, Acc2);
+pre_order_traverse2({none, Val, Right}, F, Acc) ->
+    Acc2 = F(Val, Acc),
+    pre_order_traverse2(Right, F, Acc2);
+pre_order_traverse2({Left, Val, Right}, F, Acc) ->
+    Acc2 = pre_order_traverse2(Left, F, Acc),
+    Acc3 = F(Val, Acc2),
+    pre_order_traverse2(Right, F, Acc3).
+
+
+add(Val, {bst, CmpFn, Nodes}) ->
+    {bst, CmpFn, add(Val, CmpFn, Nodes)}.
+
+add(Val, _CmpFn, none) ->
+    {none, Val, none};
+add(Val, CmpFn, {Left, NodeVal, Right} = T) ->
+    case CmpFn(Val, NodeVal) of
+        -1 ->
+            {add(Val, CmpFn, Left), NodeVal, Right};
+        0 ->
+            T;
+        1 ->
+            {Left, NodeVal, add(Val, CmpFn, Right)}
+    end.
--- examples/erlang/ctci/bst/test/bst_tests.erl
+++ examples/erlang/ctci/bst/test/bst_tests.erl
@@ -18,6 +18,32 @@ setup() ->
 teardown(_) ->
     ok.
 
+        int_cmp(A, B) when A > B ->
+                    -1;
+        int_cmp(A, B) when A == B ->
+                    0;
+        int_cmp(A, B) when A < B ->
+                    1.
+
 
 length_test() ->
-    ok.
+    T = bst:new(fun int_cmp/2),
+    ?assertEqual(0, bst:length(T)),
+    Nums = [X || {_, X} <- lists:sort([{random:uniform(), N} || N <- lists:seq(1, 10)])],
+    %% ?debugFmt("Nums=~p", [Nums]),
+    T2 = bst:add(99, T),
+    %% ?debugFmt("T2=~p", [T2]),
+    ?assertEqual(1, bst:length(T2)),
+    T3 = lists:foldl(fun (N, T) -> bst:add(N, T) end, T, Nums),
+    %% ?debugFmt("T3=~p", [T3]),
+    ?assertEqual(10, bst:length(T3)).
+
+pre_order_traverse_test() ->
+    Nums = lists:seq(1,10),
+    T = lists:foldl(fun (N, T0) -> bst:add(N, T0) end, bst:new(fun int_cmp/2), Nums),
+    ?debugFmt("T=~p", [T]),
+    Nums2 = bst:pre_order_traverse(
+              T,
+              fun (N, Acc) -> [N | Acc] end,
+              []),
+    ?assertEqual(Nums, Nums2).
