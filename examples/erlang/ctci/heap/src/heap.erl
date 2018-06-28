453c9ba8c71c790738eee35ca37ba6688c376863
Kyle Burton <kyle.burton@gmail.com>
2018-06-28 14:08:49 -0700

    wip: still need to impl/fix down_heap

--- examples/erlang/ctci/heap/src/heap.erl
+++ examples/erlang/ctci/heap/src/heap.erl
@@ -12,118 +12,171 @@
 -include("heap.hrl").
 
 new(CmpFn) ->
-  #heap{cmp_fn=CmpFn}.
+    #heap{cmp_fn=CmpFn}.
 
 
 length(#heap{buff=Buff}) ->
-  array:size(Buff).
+    array:size(Buff).
 
 insert(Val, #heap{buff=Buff, cmp_fn=CmpFn} = H) ->
-  %% add to tree, keepig it balanced
-  Pos = array:size(Buff),
-  Buff2 = array:set(Pos, Val, Buff),
-  H#heap{buff=up_heap(Pos, CmpFn, Buff2)}.
+    %% add to tree, keepig it balanced
+    Pos = array:size(Buff),
+    Buff2 = array:set(Pos, Val, Buff),
+    H#heap{buff=up_heap(Pos, CmpFn, Buff2)}.
 
 is_odd(N) ->
-  N rem 2 =:= 1.
+    N rem 2 =:= 1.
 
 left_pos(P) ->
-  P*2 + 1.
+    P*2 + 1.
+
+left_pos(P, Buff) ->
+    P2 = left_pos(P),
+    {P2, P2 < array:size(Buff)}.
 
 right_pos(P) ->
-  P*2 + 2.
+    P*2 + 2.
+
+right_pos(P, Buff) ->
+    P2 = right_pos(P),
+    {P2, P2 < array:size(Buff)}.
 
 parent_pos(Pos) ->
-  %% lpos = pos*2+1
-  %% rpos = pos*2+2
-  %% 0: l=1; r=2
-  %% 1: l=3; r=4
-  %% 2: l=5; r=6
-  %% 3: l=7; r=8
-  %% 4: l=9; r=10
-  %% left is always odd, right is always even
-  case is_odd(Pos) of
-    true ->
-        (Pos-1) div 2;
-    false ->
-        (Pos-2) div 2
-  end.
+    %% lpos = pos*2+1
+    %% rpos = pos*2+2
+    %% 0: l=1; r=2
+    %% 1: l=3; r=4
+    %% 2: l=5; r=6
+    %% 3: l=7; r=8
+    %% 4: l=9; r=10
+    %% left is always odd, right is always even
+    case is_odd(Pos) of
+        true ->
+            (Pos-1) div 2;
+        false ->
+            (Pos-2) div 2
+    end.
 
 up_heap(0, _CmpFn, Buff) ->
-  Buff;
+    Buff;
 up_heap(Pos, CmpFn, Buff) ->
-  PPos = parent_pos(Pos),
-  Val = array:get(Pos, Buff),
-  PVal = array:get(PPos, Buff),
-  case CmpFn(Val, PVal) of
-    -1 ->
-      Buff2 = array:set(Pos, PVal, Buff),
-      Buff3 = array:set(PPos, Val, Buff2),
-      up_heap(PPos, CmpFn, Buff3);
-    _ ->
-      Buff
-  end.
+    PPos = parent_pos(Pos),
+    Val = array:get(Pos, Buff),
+    PVal = array:get(PPos, Buff),
+    case CmpFn(Val, PVal) of
+        -1 ->
+            Buff2 = array:set(Pos, PVal, Buff),
+            Buff3 = array:set(PPos, Val, Buff2),
+            up_heap(PPos, CmpFn, Buff3);
+        _ ->
+            Buff
+    end.
 
 reduce(Fn2, Acc, #heap{buff=Buff}) ->
-  reduce(Fn2, Acc, Buff, 0).
+    reduce(Fn2, Acc, Buff, 0).
 
 reduce(Fn2, Acc0, Buff, Pos) ->
-  case Pos >= array:size(Buff) of
-    true ->
-      Acc0;
-    false ->
-      Acc2 = Fn2(array:get(Pos, Buff), Acc0),
-      Acc3 = reduce(Fn2, Acc2, Buff, left_pos(Pos)),
-      reduce(Fn2, Acc3, Buff, right_pos(Pos))
-  end.
+    case Pos >= array:size(Buff) of
+        true ->
+            Acc0;
+        false ->
+            Acc2 = Fn2(array:get(Pos, Buff), Acc0),
+            Acc3 = reduce(Fn2, Acc2, Buff, left_pos(Pos)),
+            reduce(Fn2, Acc3, Buff, right_pos(Pos))
+    end.
 
 position_pairs(#heap{} = H) ->
-  position_pairs(H#heap.buff, 0).
+    position_pairs(H#heap.buff, 0).
 
 position_pairs(Buff, Pos) ->
-  position_pairs(Buff, Pos, left_pos(Pos), right_pos(Pos), []).
+    position_pairs(Buff, Pos, left_pos(Pos), right_pos(Pos), []).
 
 position_pairs(Buff, Pos, LPos, RPos, Acc0) ->
-  io:format("position_pairs: Buff=~p", [Buff]),
-  io:format("position_pairs: Pos=~p; LPos=~p; RPos=~p; size=~p~n",
-            [Pos, LPos, RPos, array:size(Buff)]),
-  case {LPos < array:size(Buff), RPos < array:size(Buff)} of
-    {true, true} ->
-      Acc1 = [{Pos, LPos} | Acc0],
-      Acc2 = [{Pos, RPos} | Acc1],
-      Acc3 = position_pairs(Buff, LPos, left_pos(LPos), right_pos(LPos), Acc2),
-      position_pairs(Buff, RPos, left_pos(RPos), right_pos(RPos), Acc3);
-    {true, false} ->
-      Acc1 = [{Pos, LPos} | Acc0],
-      position_pairs(Buff, LPos, left_pos(LPos), right_pos(LPos), Acc1);
-    {false, true} ->
-      Acc1 = [{Pos, RPos} | Acc0],
-      position_pairs(Buff, RPos, left_pos(RPos), right_pos(RPos), Acc1);
-    _ ->
-      Acc0
-  end.
+    io:format("position_pairs: Buff=~p", [Buff]),
+    io:format("position_pairs: Pos=~p; LPos=~p; RPos=~p; size=~p~n",
+              [Pos, LPos, RPos, array:size(Buff)]),
+    case {LPos < array:size(Buff), RPos < array:size(Buff)} of
+        {true, true} ->
+            Acc1 = [{Pos, LPos} | Acc0],
+            Acc2 = [{Pos, RPos} | Acc1],
+            Acc3 = position_pairs(Buff, LPos, left_pos(LPos), right_pos(LPos), Acc2),
+            position_pairs(Buff, RPos, left_pos(RPos), right_pos(RPos), Acc3);
+        {true, false} ->
+            Acc1 = [{Pos, LPos} | Acc0],
+            position_pairs(Buff, LPos, left_pos(LPos), right_pos(LPos), Acc1);
+        {false, true} ->
+            Acc1 = [{Pos, RPos} | Acc0],
+            position_pairs(Buff, RPos, left_pos(RPos), right_pos(RPos), Acc1);
+        _ ->
+            Acc0
+    end.
 
 to_dotty(#heap{buff=Buff} = H) ->
-  Pairs0 = [{L, array:get(L, Buff), R,array:get(R, Buff)} || {L,R} <- position_pairs(H)],
-  Pairs1 = [io_lib:format("\"~p=~p\" -> \"~p=~p\"", [Lp, L, Rp, R]) || {Lp, L, Rp, R} <- Pairs0],
-  "digraph {\n" ++
-  string:join(Pairs1, ";\n  ") ++
-  "\n}\n".
+    Pairs0 = [{L, array:get(L, Buff), R,array:get(R, Buff)} || {L,R} <- position_pairs(H)],
+    Pairs1 = [io_lib:format("\"~p=~p\" -> \"~p=~p\"", [Lp, L, Rp, R]) || {Lp, L, Rp, R} <- Pairs0],
+    "digraph {\n" ++
+        string:join(Pairs1, ";\n  ") ++
+        "\n}\n".
+
+swap(P1, P2, Buff0) ->
+    io:format("~p:swap: P1=~p; P2=~p; Buff0=~p~n", [?MODULE, P1, P2, Buff0]),
+    Tmp = array:get(P1, Buff0),
+    Buff1 = array:set(P1, array:get(P2, Buff0), Buff0),
+    array:set(P2, Tmp, Buff1).
 
 
-down_heap(0, _CmpFn, Buff) ->
-  Buff;
+%% down_heap happens after a pop/remove
+%% pop start's w/the head, down_heap just calls this 'node'
+%% if the node has no children, we're done
+%% if the node has one child, compare & swap if necessary
+%% if the node has two children, swap w/least child & recurse
 down_heap(Pos, CmpFn, Buff) ->
-  %% compare Pos to it's children, if in the correct order, we're done
-  %% otherwise swap with the lesser child
-  Buff.
+    io:format("~p:down_heap: Pos=~p; Buff=~p~n", [?MODULE, Pos, Buff]),
+    {Lpos, Lvalid} = left_pos(Pos, Buff),
+    {Rpos, Rvalid} = right_pos(Pos, Buff),
+    io:format("~p:down_heap: L={~p,~p} R={~p,~p}~n", [?MODULE, Lpos,Lvalid, Rpos,Rvalid]),
+    case {Lvalid, Rvalid} of 
+        {false, false} ->
+            Buff;
+        {true, false} ->
+            %% NB: only swap if we need to
+            swap(Pos, Lpos, Buff);
+        {false, true} ->
+            %% NB: only swap if we need to
+            swap(Pos, Rpos, Buff);
+        {true, true} ->
+            Lval = array:get(Lpos, Buff),
+            Rval = array:get(Rpos, Buff),
+            case CmpFn(Lval, Rval) of
+                -1 ->
+                    down_heap(Lpos, CmpFn, swap(Pos, Lpos, Buff));
+                0 ->
+                    Buff;
+                1 -> 
+                    down_heap(Rpos, CmpFn, swap(Pos, Rpos, Buff))
+            end
+    end.
+
+%% compare Pos to it's children, if in the correct order, we're done
+%% otherwise swap with the lesser child
+%% V = array:get(Pos, Buff),
+%% L = array:get(left_pos(Pos), Buff),
+%% R = array:get(right_pos(Pos), Buff),
 
 pop(#heap{buff=Buff0, cmp_fn=CmpFn} = H) ->
-  TopVal = array:get(0, Buff0),
-  LastVal = array:get(array:size(Buff0) - 1, Buff0),
-  Buff1 = array:set(0, LastVal, Buff0),
-  Buff2 = array:resize(array:size(Buff0) - 1, Buff1),
-  Buff3 = down_heap(0, CmpFn, Buff2),
-  {TopVal, H#heap{buff=Buff3}}.
+    io:format("~p:pop: Buff0=~p~n", [?MODULE, Buff0]),
+    case array:size(Buff0) of
+        0 ->
+            throw(empty_heap);
+        1 ->
+            TopVal = array:get(0, Buff0),
+            {TopVal, H#heap{buff=array:resize(0, Buff0)}};
+        _ ->
+            TopVal = array:get(0, Buff0),
+            LastVal = array:get(array:size(Buff0) - 1, Buff0),
+            Buff1 = array:set(0, LastVal, Buff0),
+            Buff2 = array:resize(array:size(Buff1) - 1, Buff1),
+            {TopVal, H#heap{buff=down_heap(0, CmpFn, Buff2)}}
+    end.
 
 
--- examples/erlang/ctci/heap/test/heap_tests.erl
+++ examples/erlang/ctci/heap/test/heap_tests.erl
@@ -7,90 +7,160 @@
 -include_lib("eunit/include/eunit.hrl").
 
 bst_array_test_() ->
-  {foreach,
-   fun setup/0,
-   fun teardown/1,
-   []}.
+    {foreach,
+     fun setup/0,
+     fun teardown/1,
+     []}.
 
 setup() ->
-  <<I1:32/unsigned-integer, I2:32/unsigned-integer, I3:32/unsigned-integer>> = crypto:strong_rand_bytes(12),
-  rand:seed(exsplus, {I1, I2, I3}),
-  ok.
+    <<I1:32/unsigned-integer, I2:32/unsigned-integer, I3:32/unsigned-integer>> = crypto:strong_rand_bytes(12),
+    rand:seed(exsplus, {I1, I2, I3}),
+    ok.
 
 teardown(_) ->
-  ok.
+    ok.
 
 
 min_int_cmp(A, B) when A < B ->
-  -1;
+    -1;
 min_int_cmp(A, B) when A == B ->
-  0;
+    0;
 min_int_cmp(A, B) when A > B ->
-  1.
+    1.
 
 max_int_cmp(A, B) ->
-  min_int_cmp(A,B) * -1.
+    min_int_cmp(A,B) * -1.
 
 random_heap(CmpFn, Elts) ->
-  Elts2 = [X || {_, X} <- lists:sort([{rand:uniform(), N} || N <- Elts])],
-  H = heap:new(CmpFn),
-  lists:foldl(fun (E, H0) -> heap:insert(E, H0) end, H, Elts2).
+    Elts2 = [X || {_, X} <- lists:sort([{rand:uniform(), N} || N <- Elts])],
+    H = heap:new(CmpFn),
+    lists:foldl(fun (E, H0) -> heap:insert(E, H0) end, H, Elts2).
 
 length_test() ->
-  H = heap:new(fun max_int_cmp/2),
-  ?assertEqual(0, heap:length(H)),
-  H2 = heap:insert(33, H),
-  ?assertEqual(1, heap:length(H2)).
-  %% ?debugFmt("H2=~p", [H2]).
+    H = heap:new(fun max_int_cmp/2),
+    ?assertEqual(0, heap:length(H)),
+    H2 = heap:insert(33, H),
+    ?assertEqual(1, heap:length(H2)).
+%% ?debugFmt("H2=~p", [H2]).
 
 reduce_test() ->
-  H = random_heap(fun max_int_cmp/2, lists:seq(1, 15)),
-  Sum = heap:reduce(
-          fun (Val, Acc) ->
-              ?debugFmt("reduce_test#fn: Val=~p", [Val]),
-              Val + Acc
-          end,
-          0,
-          H),
-  ?assertEqual(Sum, 120).
+    H = random_heap(fun max_int_cmp/2, lists:seq(1, 15)),
+    Sum = heap:reduce(
+            fun (Val, Acc) ->
+                    ?debugFmt("reduce_test#fn: Val=~p", [Val]),
+                    Val + Acc
+            end,
+            0,
+            H),
+    ?assertEqual(Sum, 120).
 
 print_test() ->
-  H0 = random_heap(fun max_int_cmp/2, []),
-  P0 = heap:position_pairs(H0),
-  ?debugFmt("H0=~p; P0=~p", [array:to_list(H0#heap.buff), P0]),
+    H0 = random_heap(fun max_int_cmp/2, []),
+    P0 = heap:position_pairs(H0),
+    ?debugFmt("H0=~p; P0=~p", [array:to_list(H0#heap.buff), P0]),
 
-  H1 = random_heap(fun max_int_cmp/2, [1]),
-  P1 = heap:position_pairs(H1),
-  ?debugFmt("H1=~p; P1=~p", [array:to_list(H1#heap.buff), P1]),
+    H1 = random_heap(fun max_int_cmp/2, [1]),
+    P1 = heap:position_pairs(H1),
+    ?debugFmt("H1=~p; P1=~p", [array:to_list(H1#heap.buff), P1]),
 
-  H2 = random_heap(fun max_int_cmp/2, [1, 2]),
-  P2 = heap:position_pairs(H2),
-  ?debugFmt("H2=~p; P2=~p", [array:to_list(H2#heap.buff), P2]),
+    H2 = random_heap(fun max_int_cmp/2, [1, 2]),
+    P2 = heap:position_pairs(H2),
+    ?debugFmt("H2=~p; P2=~p", [array:to_list(H2#heap.buff), P2]),
 
-  H3 = random_heap(fun max_int_cmp/2, [1, 2, 3]),
-  P3 = heap:position_pairs(H3),
-  ?debugFmt("H3=~p; P3=~p", [array:to_list(H3#heap.buff), P3]),
-  
-  H4 = random_heap(fun max_int_cmp/2, lists:seq(1, 15)),
-  P4 = heap:position_pairs(H4),
-  ?debugFmt("H4=~p; P4=~p", [array:to_list(H4#heap.buff), P4]),
-  
-  ok.
+    H3 = random_heap(fun max_int_cmp/2, [1, 2, 3]),
+    P3 = heap:position_pairs(H3),
+    ?debugFmt("H3=~p; P3=~p", [array:to_list(H3#heap.buff), P3]),
+
+    H4 = random_heap(fun max_int_cmp/2, lists:seq(1, 15)),
+    P4 = heap:position_pairs(H4),
+    ?debugFmt("H4=~p; P4=~p", [array:to_list(H4#heap.buff), P4]),
+
+    ok.
 
 randlist(Size) ->
-  %% trunc()
-  [trunc(rand:uniform()*100) || _X <- lists:seq(1, Size)].
+    %% trunc()
+    [trunc(rand:uniform()*100) || _X <- lists:seq(1, Size)].
 
 dotty_test() ->
-  lists:foreach(fun gen_dotty/1, lists:seq(1, 15)),
-  %% 1 = 2.
-  ok.
+    lists:foreach(fun gen_dotty/1, lists:seq(1, 15)),
+    %% 1 = 2.
+    ok.
 
 gen_dotty(N) ->
-  H0 = random_heap(fun max_int_cmp/2, lists:seq(1, N)),
-  %% H0 = random_heap(fun max_int_cmp/2, randlist(N)),
-  Data = heap:to_dotty(H0),
-  Fname = io_lib:format("../dotty_test-~2..0B.dot", [N]),
-  ?debugFmt("Fname=~s", [Fname]),
-  file:write_file(Fname, Data),
-  ?debugFmt("wrote N=~p; Fname=~s; size=~p", [N, Fname, heap:length(H0)]).
+    Fname = io_lib:format("../dotty_test-~2..0B.dot", [N]),
+    heap_to_dot_file(Fname, random_heap(fun max_int_cmp/2, randlist(N))).
+
+heap_to_dot_file(Fname, H) ->
+    Data = heap:to_dotty(H),
+    ?debugFmt("Fname=~s", [Fname]),
+    file:write_file(Fname, Data),
+    ?debugFmt("wrote Fname=~s; size=~p", [Fname, heap:length(H)]).
+
+
+enable_tracing(Specs) ->
+    %% dbg:start(),
+    dbg:tracer(),
+    dbg:p(all, [c]),
+    enable_traces(Specs).
+
+%% https://stackoverflow.com/questions/34658714/how-to-debug-erlang-code-during-rebar3-eunit
+%% eunit:test(your_test_module,[verbose]).
+enable_traces([{M,F} | Rest]) ->
+    ?debugFmt("ENABLE TRACING: ~p:~p", [M, F]),
+    dbg:tpl(M, F, [cx]),
+    enable_traces(Rest);
+enable_traces([M| Rest]) ->
+    ?debugFmt("ENABLE TRACING: ~p:*", [M]),
+    dbg:tpl(M, [cx]),
+    enable_traces(Rest);
+enable_traces([]) ->
+    ok.
+
+
+pop_test() ->
+    %% enable_tracing([heap]),
+
+    %% NB: this should throw
+    %% HEmpty = heap:new(fun max_int_cmp/2),
+    %% {Val, HEmpty2} = heap:pop(),
+    H1 = random_heap(fun max_int_cmp/2, [137]),
+    ?assertEqual(1, heap:length(H1)),
+    ?debugFmt("pre-pop: H1=~p", [H1]),
+    {Val, H2} = heap:pop(H1),
+    ?debugFmt("post-pop: H2=~p", [H2]),
+    ?assertEqual(0, heap:length(H2)),
+    ?assertEqual(137, Val),
+
+    H3 = random_heap(fun max_int_cmp/2, [11, 22]),
+    ?assertEqual(2, heap:length(H3)),
+    ?debugFmt("pre-pop: H3=~p", [H3]),
+    {Val2, H4} = heap:pop(H3),
+    ?debugFmt("post-pop: H4=~p", [H4]),
+    ?assertEqual(1, heap:length(H4)),
+    ?assertEqual(22, Val2),
+
+    H5 = random_heap(fun max_int_cmp/2, [11, 22, 33]),
+    ?assertEqual(3, heap:length(H5)),
+    ?debugFmt("pre-pop: H3=~p", [H5]),
+    {Val3, H6} = heap:pop(H5),
+    ?debugFmt("post-pop: H4=~p", [H6]),
+    ?assertEqual(2, heap:length(H6)),
+    ?assertEqual(33, Val3),
+    ok.
+
+pop_visual_test() ->
+    H0  = random_heap(fun max_int_cmp/2, randlist(15)),
+    H0a = lists:foldl(fun(N, H1) ->
+                              BFname = io_lib:format("../pop_test-before-~2..0B.dot", [N]),
+                              heap_to_dot_file(BFname, H1),
+                              {Val, H2} = heap:pop(H1),
+                              ?debugFmt("pop_test: N=~p Val=~p", [N, Val]),
+                              AFname = io_lib:format("../pop_test-after-~2..0B.dot", [N]),
+                              heap_to_dot_file(AFname, H2),
+                              H2
+                      end,
+                      H0,
+                      lists:seq(1, 5)),
+
+    ok.
+
