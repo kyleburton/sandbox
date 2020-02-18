48fa35438ee60939ce96ab983df65173c23610d5
Kyle Burton <kyle.burton@gmail.com>
2020-02-17 22:47:23 -0800

    serde example

--- examples/rust/reqwest-sandbox/src/main.rs
+++ examples/rust/reqwest-sandbox/src/main.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 
+// extern crate rustc_serialize;
+// use rustc_serialize::json;
+extern crate serde_json;
+
 // http://localhost.localdomain.com:5080/tix/js/version.js
 // http://localhost.localdomain.com:5080/version.js
 
@@ -34,6 +38,16 @@ async fn test3(url: &'static str) -> Result<std::collections::HashMap<String, St
     Ok(resp)
 }
 
+#[tokio::main]
+async fn get_as_text(url: &'static str) -> Result<String, Box<dyn std::error::Error>> {
+    let resp = reqwest::Client::builder().build()?.get(url).send().await?;
+    return Ok(resp.text().await?)
+}
+
+fn parse_json_to_map(data: & std::string::String) -> serde_json::Value {
+    return serde_json::from_str::<serde_json::Value>(data).unwrap()
+}
+
 fn main () {
     let res1 = test1();
     println!("test1: res={:#?}", res1);
@@ -42,4 +56,17 @@ fn main () {
 
     println!("should 404    res={:#?}", test2("http://localhost.localdomain.com:5080/version.js"));
     println!("should 404    res={:#?}", test3("http://localhost.localdomain.com:5080/version.js"));
+
+    let res3 = get_as_text("http://localhost.localdomain.com:5080/tix/js/version.js");
+    println!("get_as_text={:#?}", res3);
+    let body1 = match res3 {
+        Ok(text) => text,
+        Err(err) => {
+            println!("error: {:#?}", err);
+            "error!".to_string()
+        },
+    };
+    println!("    text={:#?}", body1);
+    println!("    json={:#?}", parse_json_to_map(&body1));
+
 }
