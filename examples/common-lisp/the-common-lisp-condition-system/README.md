3424b8765cbd7543c2c87f3e65b110486a81a41c
Kyle Burton <kyle.burton@gmail.com>
2020-12-06 23:29:13 -0800

    wip: more fun w/the book, conditions and restarts

---
+++ examples/common-lisp/the-common-lisp-condition-system/README.md
@@ -0,0 +1,6 @@
+The Common Lisp Condition System
+
+
+```
+https://astraybi.wordpress.com/2015/08/02/how-to-install-slimesbclquicklisp-into-emacs/
+```
--- examples/common-lisp/the-common-lisp-condition-system/ch02.lisp
+++ examples/common-lisp/the-common-lisp-condition-system/ch02.lisp
@@ -10,6 +10,8 @@
 
 (defvar *hooks* '())
 
+(defvar *csgo-launched-p* nil)
+
 (defun ensure-csgo-launched (person)
   (when (member :csgo person)
     (unless *csgo-launched-p*
@@ -20,7 +22,6 @@
   (format t ";; Calling ~A.~%" (first person)))
 
 
-(defvar *csgo-launched-p* nil)
 
 (defun call-people ()
   (setf *csgo-launched-p* nil)
@@ -157,6 +158,182 @@
       (call-people3)))
   )
 
+(define-condition grave-mistake (error)
+  ((%reason :reader reason :initarg :reason)))
+
+'(
+  (reason
+   (make-condition 'grave-mistake :reason "some stuff"))
+
+  )
+
+(defun receive-phone-call (person)
+  (format t ";; Answering a call from ~A.~%" (first person))
+  (when (member :ex person)
+    (format t ";; About to commit a grave mistake...~%")
+    (signal 'grave-mistake :reason :about-to-call-your-ex)
+    ;; (we do not want to be here)
+    ))
+
+(defun defuse-error (condition)
+  (declare (ignore condition))
+  (format t ";; Nope nope nope, not answering!~%")
+  (throw :do-not-answer nil))
+
+(defun defuse-grave-mistake (condition)
+  (let ((reason (reason condition)))
+    (format t ";; Nope nope nope, not answering - reason was, ~A!~%" reason))
+  (throw :do-not-answer nil))
+
+'(
+
+  (handler-bind ((error #'defuse-error))
+    (dolist (person *phonebook*)
+      (catch :do-not-answer
+        (receive-phone-call person))))
+
+  )
+
+
+(defun receive-phone-call2 (person)
+  (format t ";; Answering a call from ~A.~%" (first person))
+  (when (member :ex person)
+    (error 'grave-mistake :reason :about-to-call-your-ex)
+    ;; (format t ";; we will never get here.~%")
+    ))
+
+
+'(
+
+  ;; NB: this doesn't allow a restart or return
+  ;; reading through:
+  ;;  https://lispcookbook.github.io/cl-cookbook/error_handling.html#restarts-interactive-choices-in-the-debugger
+  (handler-bind ((grave-mistake (lambda (condition)
+                                  (declare (ignore condition))
+                                  (format t ";;    allowing the grave mistake ...~%"))))
+    (dolist (person *phonebook*)
+      (catch :do-not-answer
+        (receive-phone-call2 person))))
+
+  (let ((val 3))
+    (assert (zerop val)
+            (val)
+            "val must be zero, please change it.")
+    :ok)
+
+  (handler-bind ((grave-mistake (lambda (condition)
+                                  (declare (ignore condition))
+                                  (throw :do-not-answer nil))))
+    (dolist (person *phonebook*)
+      (catch :do-not-answer
+        (receive-phone-call2 person))))
+
+  (progn
+    (format t "~%")
+    (dolist (person *phonebook*)
+      (let ((call-result
+              (handler-case (progn
+                              (receive-phone-call2 person)
+                              :answered)
+                (grave-mistake (condition)
+                  (format t ";; Nope, not this time: ~A~%" (reason condition))
+                  :not-answered))))
+        (format t ";;    call-result: ~A~%" call-result))))
+
+  )
+
+
+;;  https://lispcookbook.github.io/cl-cookbook/error_handling.html#restarts-interactive-choices-in-the-debugger
+(defun divide-with-restarts (x y)
+  (restart-case (/ x y)
+    (return-zero ()  ;; <-- creates a new restart called "RETURN-ZERO"
+      :report "Return 0"
+      0)
+    (divide-by-one ()
+      :report "Divide by 1"
+      (/ x 1))))
+
+
+'(
+
+  (divide-with-restarts 3 0)
+
+  )
+
+(defun prompt-new-value (prompt)
+  (format *query-io* prompt) ;; *query-io*: the special stream to make user queries.
+  (force-output *query-io*)  ;; Ensure the user sees what he types.
+  (list (read *query-io*)))   ;; We must return a list.
+
+(defun divide-with-restarts2 (x y)
+  (restart-case (/ x y)
+    (return-zero ()  ;; <-- creates a new restart called "RETURN-ZERO"
+      :report "Return 0"
+      0)
+    (divide-by-one ()
+      :report "Divide by 1"
+      (/ x 1))
+    (set-new-divisor (value)
+      :report "Enter a new divisor"
+      :interactive (lambda () (prompt-new-value "Please enter a new divisor: "))
+      (divide-with-restarts x value))))
+
+'(
+
+  (divide-with-restarts2 3 0)
+
+  )
+
+
+(defun prompt-new-value2 (prompt)
+  (list
+   (let ((input
+           ;; We capture the program's output to a string.
+           (with-output-to-string (s)
+             (let* ((*standard-output* s))
+               (uiop:run-program `("zenity"
+                                   "--forms"
+                                   ,(format nil "--add-entry=~a" prompt))
+                                 :output s)))))
+     ;; We get a string and we want a number.
+     ;; We could also use parse-integer, the parse-number library, etc.
+     (read-from-string input))))
+
+;; (prompt-new-value2 "thing: ")
+
+'(
+
+  (divide-with-restarts2 3 0)
+
+
+  )
+
+(defun divide-with-restarts3 (x y)
+  (restart-case (/ x y)
+    (return-zero () ;; <-- creates a new restart called "RETURN-ZERO"
+      :report "Return 0"
+      0)
+    (divide-by-one ()
+      :report "Divide by 1"
+      (/ x 1))
+    (set-new-divisor (value)
+      :report "Enter a new divisor"
+      :interactive (lambda () (prompt-new-value2 "Please enter a new divisor: "))
+      (divide-with-restarts x value))))
+
+(defun divide-and-handle-error (x y)
+  (handler-bind
+      ((division-by-zero (lambda (c)
+                           (format t "Got error: ~a~%" c) ;; error-message
+                           (format t "and will divide by 1~&")
+                           (invoke-restart 'divide-by-one))))
+    (divide-with-restarts3 x y)))
+
+'(
+
+  (divide-and-handle-error 3 0)
+
+  )
 
 
 ;; Local Variables:
