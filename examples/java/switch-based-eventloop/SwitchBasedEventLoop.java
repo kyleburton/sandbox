16fdd29e5ad6dc7e20947a3aee88eb0f1112adb8
Kyle Burton <kyle.burton@gmail.com>
2021-09-03 14:26:38 -0700

    example

---
+++ examples/java/switch-based-eventloop/SwitchBasedEventLoop.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * java.util.Scanner inp = new java.util.Scanner(System.in);
+ *
+ *
+ */
+public class SwitchBasedEventLoop {
+    public enum Locations {
+        FRONT_PORCH,
+        ENTRYWAY,
+        DEN,
+        LIVING_ROOM,
+    }
+
+    public static void checkQuit(String action) {
+        if (action.equals("quit") || action.equals("exit") || action.equals("q")) {
+            System.out.println("ok, goodbye and thanks for playing!");
+            System.exit(0);
+        }
+    }
+
+    public static void main (String [] args) {
+        Locations playerLocation = Locations.FRONT_PORCH;
+        java.util.Scanner inp = new java.util.Scanner(System.in);
+        String action;
+
+        while (true) {
+            switch (playerLocation) {
+                case FRONT_PORCH:
+                    System.out.println("");
+                    System.out.println("You find yoruself standing the front door to a house, you see a doorbell and a welcome mat.");
+                    action = inp.nextLine();
+                    checkQuit(action);
+                    if (action.equals( "ring bell") || action.equals("ring doorbell") || action.equals("ring")) {
+                        System.out.println("you ring the bell, the door mysteriously swings open and you step in side ...");
+                        playerLocation = Locations.ENTRYWAY;
+                        break;
+                    }
+                    else {
+                        System.out.println("I'm not sure you can do that here ...");
+                    }
+                    break;
+                case ENTRYWAY:
+                    System.out.println("");
+                    System.out.println("You're in the entryway of the house, you can see a door to your right that leads into the den, and the living room.  You can go into the den (den, door, right, north) or the living room (living room, forward, west).");
+                    action = inp.nextLine();
+                    checkQuit(action);
+                    if (action.equals("den")) {
+                        System.out.println("you step down a few stairs and into the den ...");
+                        playerLocation = Locations.DEN;
+                        break;
+                    }
+                    else {
+                        System.out.println("I'm not sure you can do that here ...");
+                    }
+                    break;
+                default:
+                    System.out.println("oh no, I'm not sure where you are or how you got here ... your feel lightheaded and dizzy");
+                    System.out.println(" ... you feel yourself falling as you slip unconcious ... ");
+                    playerLocation = Locations.FRONT_PORCH;
+            }
+        }
+    }
+}
