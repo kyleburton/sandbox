9a4d2881d8b4d35b62867bace239a678ba1898be
Kyle Burton <kyle.burton@gmail.com>
2021-12-19 22:47:57 -0800

    dev-setup: for virtualbox exteral setup up to git clone of sandbox

--- dev-setup/files/setup/vbox-local-bootstrap.sh
+++ dev-setup/files/setup/vbox-local-bootstrap.sh
@@ -2,26 +2,42 @@
 set -eEuo pipefail
 set -x
 
-set -x
-test -f /usr/share/virtualbox/VBoxGuestAdditions.iso \
-  || sudo apt-get install build-essential dkms virtualbox-guest-additions-iso
 
-echo "TODO: install homebrew"
+function apt-install {
+  for pkg in $@; do
+    if ! apt list --installed | grep "^$pkg"; then
+      # https://askubuntu.com/questions/17823/how-to-list-all-installed-packages
+      # aptitude search '~i!~M'
+      sudo apt install "$pkg"
+    fi
+  done
+}
 
-if [[ ! -f $HOME/.linuxbrew/bin/brew  ]]; then 
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
 
-test -d "$HOME/code/github.com/kyleburton" || mkdir -p "$HOME/code/github.com/kyleburton"
+case "${1:-}" in
+  part2)
+    if [[ ! -f "$HOME/code/github.com/kyleburton/sandbox" ]]; then
+      GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+        git clone \
+        git@github.com:kyleburton/sandbox.git \
+        "$HOME/code/github.com/kyleburton/sandbox"
+    fi
+    ;;
+  *)
+    test -f /usr/share/virtualbox/VBoxGuestAdditions.iso \
+      || sudo apt-get install build-essential dkms virtualbox-guest-additions-iso
+    
+    echo "TODO: install homebrew"
+    
+    if [[ ! -f $HOME/.linuxbrew/bin/brew  ]]; then 
+      bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+    
+    test -d "$HOME/code/github.com/kyleburton" || mkdir -p "$HOME/code/github.com/kyleburton"
+    
+    apt-install git aptitude rsync openssh-server
+  ;;
+esac
 
-if ! apt list --installed | grep ^git; then
-  sudo apt install git
-fi
 
-if [[ -f "$HOME/code/github.com/kyleburton/sandbox" ]]; then
-  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-    git clone \
-    git@github.com:kyleburton/sandbox.git \
-    "$HOME/code/github.com/kyleburton/sandbox"
-fi
 
