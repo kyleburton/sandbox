a34c7221923c2eff4e9dae78ce772a1ff67bea3c
Kyle Burton <kyle.burton@gmail.com>
2022-01-10 21:43:09 -0800

    scratch

--- examples/java/scratch/Project20220106.java
+++ examples/java/scratch/Project20220106.java
@@ -6,7 +6,11 @@ import algorithm.OrderedNums;
 
 /*
 
-  You will be given a class which on creation of an object of that class generates a list of numbers contained in an int array.  That array may be retrieved by calling the getList() method from the object.  You will take the numbers in that array and create a new list with the following requirements:
+  You will be given a class which on creation of an object of that
+  class generates a list of numbers contained in an int array.  That
+  array may be retrieved by calling the getList() method from the
+  object.  You will take the numbers in that array and create a new
+  list with the following requirements:
 
   All the perfect squares need to be listed first
   All the prime numbers must be listed after the perfect squares
@@ -52,6 +56,21 @@ public class Project20220106 {
         for (int ii = 0; ii < myList2.length; ++ii ) {
             System.out.println(" [" + ii + "] = " + myList2[ii]);
         }
+
+        WeirdSorter sorter = new WeirdSorter(myList);
+        int [] weirdlySorted = sorter.getList();
+        System.out.println("The WeirdSorter list is " + weirdlySorted.length + " elements:");
+        for (int ii = 0; ii < myList.length; ++ii ) {
+            System.out.print(myList[ii] + ", ");
+            // System.out.println(" [" + ii + "] = " + myList[ii]);
+        }
+        System.out.println("");
+        for (int ii = 0; ii < weirdlySorted.length; ++ii ) {
+            System.out.print(weirdlySorted[ii] + ", ");
+            // System.out.println(" [" + ii + "] = " + weirdlySorted[ii]);
+        }
+        System.out.println("");
+
     }
 }
 
@@ -59,13 +78,133 @@ public class Project20220106 {
 class HasList {
     public int[] getList() {
         Random rng = new Random();
-        int listLengh = rng.nextInt(23);
+        int listLengh = 5 + rng.nextInt(23);
         int [] aList = new int[listLengh];
 
         for (int ii = 0; ii < listLengh; ++ii) {
-            aList[ii] = rng.nextInt(listLengh*2);
+            aList[ii] = 1+rng.nextInt(listLengh*2);
         }
 
         return aList;
     }
+}
+
+class WeirdSorter {
+    private int[] unsorted;
+    private int[] sorted;
+
+    public WeirdSorter(int []nums) {
+        unsorted = nums;
+        sorted = sortNums(nums);
+    }
+
+    /**
+       All the perfect squares need to be listed first
+       All the prime numbers must be listed after the perfect squares
+       All numbers whose digits add up to be a multiple of 6 and are not prime must be at the end of the list
+       All numbers that do not fall into categories 1-3 above must be listed between the prime numbers and the digits that add to multiples of 6 numbers
+       Each list must be separated by a 0.
+
+       33 => 6
+       3333 => 12
+    */
+    public int[] sortNums(int [] givenNums) {
+        int[] nums = new int[givenNums.length];
+        int[] sortedNums = new int[nums.length+3];
+        int targetPos = 0;
+
+        System.arraycopy(givenNums, 0, nums, 0, nums.length);
+
+        // first pass, grab all of the perfect squares
+        for(int ii = 0; ii < nums.length; ++ii) {
+            if (isPerfectSquare(nums[ii])) {
+                // copy it to sortedNums
+                sortedNums[targetPos++] = nums[ii];
+                // blank it out
+                nums[ii] = -1;
+            }
+        }
+
+        // append a zero
+        sortedNums[targetPos++] = 0;
+
+        // second pass, grab all of the prime numbers
+        for(int ii = 0; ii < nums.length; ++ii) {
+            if (nums[ii] == -1) {
+                continue;
+            }
+
+            if (isPrime(nums[ii])) {
+                // copy it to sortedNums
+                sortedNums[targetPos++] = nums[ii];
+                // blank it out
+                nums[ii] = -1;
+            }
+        }
+
+        // append a zero
+        sortedNums[targetPos++] = 0;
+
+        // third pass, grab all of the nums who's digits are not div by 6
+        for(int ii = 0; ii < nums.length; ++ii) {
+            if (nums[ii] == -1) {
+                continue;
+            }
+
+            if (sumDigits(nums[ii]) % 6 != 0) {
+                // copy it to sortedNums
+                sortedNums[targetPos++] = nums[ii];
+                // blank it out
+                nums[ii] = -1;
+            }
+        }
+
+        // append a zero
+        sortedNums[targetPos++] = 0;
+
+        // final pass, grab all of the remaining nums
+        for(int ii = 0; ii < nums.length; ++ii) {
+            if (nums[ii] == -1) {
+                continue;
+            }
+
+            sortedNums[targetPos++] = nums[ii];
+        }
+
+        return sortedNums;
+    }
+
+    public int sumDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+
+    public boolean isPerfectSquare(int num) {
+        int sqrt = (int)Math.sqrt(num);
+        return num != 0 && num == (sqrt*sqrt);
+    }
+
+    public boolean isPrime(int num) {
+        if (0 == num%2) {
+            return false;
+        }
+
+        int sqrt = (int)Math.sqrt(num);
+        for (int ii = 3; ii <= sqrt; ii += 2) {
+            if (0 == num%ii) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int[] getList() {
+        return sorted;
+    }
+
 }
