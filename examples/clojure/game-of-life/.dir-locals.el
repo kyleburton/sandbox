c2b453f2726c6d54bc2904f804a706cd5dcefd8c
Kyle Burton <kyle.burton@gmail.com>
2022-02-06 13:59:03 -0800

    gol: wip

--- examples/clojure/game-of-life/.dir-locals.el
+++ examples/clojure/game-of-life/.dir-locals.el
@@ -1,2 +1,24 @@
-((nil . ((cider-default-cljs-repl . shadow)
+((clojurescript-mode
+  (krb-clj-cider-connect-args :host "localhost" :port "8779" :cljs-repl-type shadow)
+  (cider-shadow-default-options . "app")
+  (krb-clj-cider-connect-fn . cider-connect-cljs)
+  (ag-ignore-list .
+                  ("vendor/bootstrap"
+                   "js/compiled"
+                   "target/stale"))
+  (ffip-project-root       . "~/code/snapclean.me/artzonewestla.com/artzone-web")
+  (krb-ag-search-directory . "~/code/snapclean.me/artzonewestla.com/artzone-web")
+  (eval . (progn
+            (require 'find-file-in-project)
+            ;; NB: the find command indicated that anything ending with a '/'
+            ;; can't possibly match
+            (add-to-list 'ffip-prune-patterns "*/vendor/bootstrap")
+            (add-to-list 'ffip-prune-patterns "*/.shadow-cljs")
+            (add-to-list 'ffip-prune-patterns "*/node_modules")
+            (add-to-list 'ffip-prune-patterns "*resources/public/js/compiled")
+            (add-to-list 'ffip-prune-patterns "*/resources/public/vendor")
+            (add-to-list 'ffip-prune-patterns "*.clj-kondo")
+            (add-to-list 'ffip-prune-patterns "*target/browser-test") ;; not working?
+            (add-to-list 'ffip-prune-patterns "target/browser-test"))))
+ (nil . ((cider-default-cljs-repl . shadow)
          (cider-shadow-cljs-default-options . "app"))))
