d52d2f003e871eb6a822e64ed4956c1e030cdf1d
Kyle Burton <kyle.burton@gmail.com>
2022-04-11 20:00:39 -0700

    drop screen

--- dev-setup/src/sh/profile.sh
+++ dev-setup/src/sh/profile.sh
@@ -6,7 +6,7 @@ function profile:install () {
     cd "$HOME"
     git clone git@github.com:kyleburton/profile.git .profile.d
     # NB: this should probably be part of the profile install process :/
-    for f in .bashrc .dictrc .editrc .gemrc .gitconfig .gitignore .profile .screenrc .tmux.conf .vimrc; do
+    for f in .bashrc .dictrc .editrc .gemrc .gitconfig .gitignore .profile .tmux.conf .vimrc; do
       test -f $f && mv $f .dist$f
     done
     )
