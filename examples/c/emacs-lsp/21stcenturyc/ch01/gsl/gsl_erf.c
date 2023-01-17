aafd17e33d3bcb35048ec50ee06151e8d2429060
Kyle Burton <kyle.burton@gmail.com>
2023-01-16 19:34:59 -0800

    wip

--- examples/c/emacs-lsp/21stcenturyc/ch01/gsl/gsl_erf.c
+++ examples/c/emacs-lsp/21stcenturyc/ch01/gsl/gsl_erf.c
@@ -1,7 +1,14 @@
 #include <gsl/gsl_cdf.h>
 #include <stdio.h>
 
+// From:
+// https://stackoverflow.com/questions/12198449/cross-platform-macro-for-silencing-unused-variables-warning/12199209#12199209
+#define MON_Internal_UnusedStringify(macro_arg_string_literal) #macro_arg_string_literal
+#define MONUnusedParameter(macro_arg_parameter) _Pragma(MON_Internal_UnusedStringify(unused(macro_arg_parameter)))
+
 int main(int argc, char **argv) {
+  MONUnusedParameter(argc)
+  MONUnusedParameter(argv)
   double bottom_tail = gsl_cdf_gaussian_P(-1.96, 1);
   printf("Area between [-1.96, 1.96]; %g\n", 1 - 2 * bottom_tail);
   return 0;
