f3f92d2c92be3c065aab054927b6e5a739fd4f5f
Kyle Burton <kyle.burton@gmail.com>
2023-04-09 15:16:06 -0700

    wip

--- examples/rust/learn-rust/display/src/main.rs
+++ examples/rust/learn-rust/display/src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+////////////////////////////////////////
 #[derive(Debug)]
 struct MinMax(i64, i64);
 
@@ -8,7 +9,9 @@ impl fmt::Display for MinMax {
         write!(f, "MinMax{{min={},max={}}}", self.0, self.1)
     }
 }
+////////////////////////////////////////
 
+////////////////////////////////////////
 #[derive(Debug)]
 struct Point2D {
     x: f64,
@@ -20,6 +23,21 @@ impl fmt::Display for Point2D {
         write!(f, "Point2D{{x={}, y={}}}", self.x, self.y)
     }
 }
+////////////////////////////////////////
+
+////////////////////////////////////////
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Complex{{real={}, imag={}i}}", self.real, self.imag)
+    }
+}
+////////////////////////////////////////
 
 fn main() {
     let minmax = MinMax(0, 14);
@@ -42,4 +60,10 @@ fn main() {
     println!("Compare points:");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+
+    let imag = Complex { real: 3.3, imag: 7.2 };
+
+    println!("Compare complex:");
+    println!("Display: {}", imag);
+    println!("Debug: {:?}", imag);
 }
