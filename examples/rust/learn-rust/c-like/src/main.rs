1278aaea76276f79c3339c61c8a12cf062411dd6
Kyle Burton <kyle.burton@gmail.com>
2023-04-09 21:46:16 -0700

    c-like

---
+++ examples/rust/learn-rust/c-like/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(dead_code)]
+
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+fn main() {
+    println!("zero is {}", Number::Zero as i32);
+    println!("one is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+}
