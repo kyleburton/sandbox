3a5073e14ad507dcae1626acd66b11a553353b93
Kyle Burton <kyle.burton@gmail.com>
2023-04-09 21:44:14 -0700

    enums

---
+++ examples/rust/learn-rust/enums/src/main.rs
@@ -0,0 +1,31 @@
+enum WebEvent {
+    PageLoad,
+    PageUnload,
+    KeyPress(char),
+    Paste(String),
+    Click { x: i64, y: i64 },
+}
+
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{}'", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\"", s),
+        WebEvent::Click {x, y} => println!("clicked at x={}, y={}", x, y),
+    }
+}
+
+fn main() {
+    let pressed = WebEvent::KeyPress('x');
+    let pasted  = WebEvent::Paste("my text".to_owned());
+    let click   = WebEvent::Click { x: 20, y:80 };
+    let load    = WebEvent::PageLoad;
+    let unload  = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+}
