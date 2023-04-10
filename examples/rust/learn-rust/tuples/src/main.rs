a49aab0d60fa0b2f7499b8844bbce17e5bba8a09
Kyle Burton <kyle.burton@gmail.com>
2023-04-09 17:40:09 -0700

    tuples

---
+++ examples/rust/learn-rust/tuples/src/main.rs
@@ -0,0 +1,57 @@
+use std::fmt;
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (int_param, bool_param) = pair;
+    return (bool_param, int_param);
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Matrix:\n({}, {})\n({}, {})\n", self.0, self.1, self.2, self.3)?;
+        write!(f, "Transpose:\n({}, {})\n({}, {})", self.0, self.2, self.1, self.3)
+    }
+
+}
+
+fn main() {
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing.
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    // Tuples can be tuple members.
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    // Tuples are printable.
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    // But long Tuples (more than 12 elements) cannot be printed.
+    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    //println!("Too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    println!("Uhe reversed pair is {:?}", reverse(pair));
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses.
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+
+    // Tuples can be destructured to create bindings.
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{}", matrix);
+}
