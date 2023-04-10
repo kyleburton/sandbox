9f163763ef3d441fa9c3c5a593027d00f85cdd45
Kyle Burton <kyle.burton@gmail.com>
2023-04-09 17:28:05 -0700

    primities

---
+++ examples/rust/learn-rust/primitves/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    let default_float = 3.0;
+    let default_integer = 7;
+
+    let mut inferred_type = 12;
+    inferred_type = 4294967296i64;
+
+    let mut mutable = 12;
+    mutable = 21;
+
+    // mutable = true;
+
+    let mutable = true;
+}
