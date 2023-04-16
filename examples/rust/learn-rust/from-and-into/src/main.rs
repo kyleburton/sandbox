e7ea4514c0dfc36e59068f68a689def871149b65
Kyle Burton <kyle.burton@gmail.com>
2023-04-16 16:53:33 -0700

    rust: try into & try from

--- examples/rust/learn-rust/from-and-into/src/main.rs
+++ examples/rust/learn-rust/from-and-into/src/main.rs
@@ -1,3 +1,5 @@
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::convert::From;
 
 #[derive(Debug)]
@@ -12,6 +14,22 @@ impl From<i32> for Number {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        }
+        else {
+            Err(())
+        }
+    }
+}
+
 fn main() {
     let num = Number::from(3);
     println!("number is: {:?}", num);
@@ -19,4 +37,13 @@ fn main() {
     let int = 5;
     let num2: Number = int.into();
     println!("num2 is: {:?}", num2);
+
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
 }
