752fdd686084c95d44ada413e0b9dff3150a453c
Kyle Burton <kyle.burton@gmail.com>
2024-01-28 22:22:05 -0800

    enable and init gmock

--- examples/c++/c++-crash-course/Project1/gtest_tests.cc
+++ examples/c++/c++-crash-course/Project1/gtest_tests.cc
@@ -1,13 +1,20 @@
 #include <gtest/gtest.h>
 #include "ch10.hpp"
+#include "gmock/gmock.h"
 
 struct AutoBrakeTest : ::testing::Test {
 	MockServiceBus bus{};
 	AutoBrake auto_brake{ bus };
 };
 
+struct MockServiceBus2 : IServiceBus {
+	MOCK_METHOD1(publish, void(const BrakeCommand& cmd));
+	MOCK_METHOD1(subscribe, void(SpeedUpdateCallback callback));
+	MOCK_METHOD1(subscribe, void(CarDetectedCallback callback));
+};
+
 TEST_F(AutoBrakeTest, InitialCarSpeedIsZero) {
-	ASSERT_FLOAT_EQ(0, auto_brake.get_speed_mps());
+	ASSERT_FLOAT_EQ(0.0, auto_brake.get_speed_mps());
 }
 
 TEST_F(AutoBrakeTest, InitialSensitivityIsFive) {
@@ -44,3 +51,21 @@ TEST_F(AutoBrakeTest, AlertWhenCollisionImminent) {
 	ASSERT_EQ(1, bus.commands_published);
 	ASSERT_DOUBLE_EQ(1L, bus.last_command.time_to_collision_s);
 }
+
+/*
+TEST_F(MockServiceBus2, TestNaggyMock) {
+	MockServiceBus2 naggy_mock;
+	::testing::NiceMock<MockServiceBus2> nice_mock;
+	::testing::StrictMock<MockServiceBus2> strict;
+}
+*/
+
+int main(int argc, char** argv) {
+	::testing::GTEST_FLAG(throw_on_failure) = true;
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::InitGoogleMock(&argc, argv);
+	// unit test as usual, Google Mock is initialized
+	return RUN_ALL_TESTS();
+}
+
+
